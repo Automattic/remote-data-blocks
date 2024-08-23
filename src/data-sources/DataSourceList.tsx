3d36de796443f6d980595053a9fb98514725303c
Shekhar Wagh <shekhar.wagh@automattic.com>
2024-08-23 23:06:55 +0530

    feat: display datasource meta in datasource list

--- src/data-sources/DataSourceList.tsx
+++ src/data-sources/DataSourceList.tsx
@@ -7,7 +7,6 @@ import {
 	PanelRow,
 	ButtonGroup,
 	Spinner,
-	Flex,
 } from '@wordpress/components';
 import { DialogInputEvent } from '@wordpress/components/src/confirm-dialog/types';
 import { useState } from '@wordpress/element';
@@ -15,7 +14,8 @@ import { __ } from '@wordpress/i18n';
 
 import AddDataSourceModal from './AddDataSourceModal';
 import { useDataSources } from './hooks/useDataSources';
-import { DataSourceType } from './types';
+import { DataSourceConfig, DataSourceType } from './types';
+import { Tag } from '../components/tag';
 import { useSettingsContext } from '../settings/hooks/useSettingsNav';
 import { toTitleCase } from '../utils/string';
 
@@ -63,6 +63,27 @@ const DataSourceList = () => {
 		return dataSources.filter( source => [ 'airtable', 'shopify' ].includes( source.service ) );
 	};
 
+	const renderDataSourceMeta = ( source: DataSourceConfig ) => {
+		if ( source.service === 'airtable' ) {
+			return (
+				<>
+					<Tag id="airtable-base" label="Base" value={ source.base.name } />
+					<Tag id="airtable-table" label="Table" value={ source.table.name } />
+				</>
+			);
+		}
+
+		if ( source.service === 'shopify' ) {
+			return (
+				<>
+					<Tag id="shopify-store" label="Store" value={ source.store } />
+				</>
+			);
+		}
+
+		return null;
+	};
+
 	return (
 		<PanelBody title={ __( 'Configure Data Sources', 'remote-data-blocks' ) }>
 			<PanelRow>
@@ -80,6 +101,7 @@ const DataSourceList = () => {
 						<tr>
 							<th style={ { textAlign: 'left' } }>{ __( 'Slug', 'remote-data-blocks' ) }</th>
 							<th style={ { textAlign: 'left' } }>{ __( 'Service', 'remote-data-blocks' ) }</th>
+							<th style={ { textAlign: 'left' } }>{ __( 'Meta', 'remote-data-blocks' ) }</th>
 							<th style={ { textAlign: 'left' } }>{ __( 'Actions', 'remote-data-blocks' ) }</th>
 						</tr>
 					</thead>
@@ -88,8 +110,15 @@ const DataSourceList = () => {
 							const { uuid, slug, service } = source;
 							return (
 								<tr key={ uuid } className="table-row">
-									<td>{ slug }</td>
-									<td>{ toTitleCase( service ) }</td>
+									<td>
+										<Text>{ slug }</Text>
+									</td>
+									<td>
+										<Text>{ toTitleCase( service ) }</Text>
+									</td>
+									<td>
+										<div className="data-source-meta">{ renderDataSourceMeta( source ) }</div>
+									</td>
 									<td>
 										<ConfirmDialog
 											isOpen={ deleteDialogOpen }
