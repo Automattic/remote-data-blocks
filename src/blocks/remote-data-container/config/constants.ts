7c36bc9fec9518731280c89a36523c8a2a4a1443
Hew <2006764+mhsdef@users.noreply.github.com>
2024-09-09 23:14:55 -0400

    Add GitHub File as HTML block (#25)
    * Initial pass
    
    * Another pass
    
    * Woop
    
    * Another pass
    
    * Happy tests
    
    * More tweaks
    
    * Simplify
    
    * Move response processing to query context
    
    * Lint
    
    * Lint
    
    * Adjust approach
    
    * Ya ya, was working on it
    
    * Add comment
    
    * Put back
    
    * Add tests
    
    * Lint
    
    * more lint
    
    * WS fix, wut.
    
    * More comments
    
    * Improve type annotations to reflect JsonObject capabilities
    
    * Add additional test cases asserting flexible response data types
    
    * Better
    
    * Fix bad merge
    
    ---------
    
    Co-authored-by: chriszarate <chris.zarate@automattic.com>

--- src/blocks/remote-data-container/config/constants.ts
+++ src/blocks/remote-data-container/config/constants.ts
@@ -5,4 +5,4 @@ export const REMOTE_DATA_CONTEXT_KEY = 'remote-data-blocks/remoteData';
 export const REMOTE_DATA_REST_API_URL = getRestUrl();
 
 export const IMAGE_FIELD_TYPES = [ 'image_alt', 'image_url' ];
-export const TEXT_FIELD_TYPES = [ 'number', 'price', 'string' ];
+export const TEXT_FIELD_TYPES = [ 'number', 'base64', 'price', 'string' ];
