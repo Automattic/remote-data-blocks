aa88197710fc8660c95fd5b245bcf0abac49e977
Hew <github@mhs.fastmail.com>
2024-09-11 18:19:41 -0400

    meh

--- example/shopify/register.php
+++ example/shopify/register.php
@@ -2,12 +2,11 @@
 
 namespace RemoteDataBlocks\Example\Shopify;
 
-use RemoteDataBlocks\Config\HttpDatasource;
+use RemoteDataBlocks\Config\ShopifyDatasource;
 use RemoteDataBlocks\Config\ShopifyGetProductQuery;
 use RemoteDataBlocks\Config\ShopifySearchProductsQuery;
 use RemoteDataBlocks\Editor\ConfigurationLoader;
 use RemoteDataBlocks\Logging\LoggerManager;
-use RemoteDataBlocks\REST\DatasourceCRUD;
 use function add_action;
 
 require_once __DIR__ . '/inc/interactivity-store/interactivity-store.php';
@@ -18,7 +17,9 @@ require_once __DIR__ . '/inc/queries/class-shopify-remove-from-cart-mutation.php
 require_once __DIR__ . '/inc/queries/class-shopify-search-products-query.php';
 
 function register_shopify_block() {
+	$block_name   = 'Shopify (hardcode example)';
 	$access_token = \RemoteDataBlocks\Example\get_access_token( 'shopify' );
+	$store_name   = 'stoph-test';
 
 	if ( empty( $access_token ) ) {
 		$logger = LoggerManager::instance();
@@ -26,43 +27,9 @@ function register_shopify_block() {
 		return;
 	}
 
-	$config = [
-		'display_name'   => 'hardcode example',
-		'uid'             => 'stoph-test',
-		'endpoint'        => 'https://stoph-test.myshopify.com/api/2024-07/graphql.json',
-		'request_headers' => [
-			'Content-Type'                      => 'application/json',
-			'X-Shopify-Storefront-Access-Token' => $access_token,
-		],
-		'image_url'       => plugins_url( './assets/shopify_logo_black.png', __FILE__ ),
-	];
-
 	$block_pattern = file_get_contents( __DIR__ . '/inc/patterns/product-teaser.html' );
 
-	register_blocks_for_shopify_data_source( $config, $block_pattern );
-
-	register_block_type( __DIR__ . '/build/blocks/shopify-cart' );
-	register_block_type( __DIR__ . '/build/blocks/shopify-cart-button' );
-
-	// Register blocks for dynamic data sources
-	// @TODO: These core example integrations should probably be moved into the parent plugin.
-	foreach ( DatasourceCRUD::get_data_sources( REMOTE_DATA_BLOCKS_SHOPIFY_SERVICE ) as $config ) {
-		// quick hack to transform data to our experimental format, which is all array based
-		$config = array_map(
-			function ( $value ) {
-				return is_object( $value ) ? (array) $value : $value;
-			},
-			(array) $config
-		);
-		register_blocks_for_shopify_data_source( $config, $block_pattern );
-	}
-}
-add_action( 'register_remote_data_blocks', __NAMESPACE__ . '\\register_shopify_block' );
-
-
-
-function register_blocks_for_shopify_data_source( array $config, string $block_pattern ): void {
-	$shopify_datasource            = new HttpDatasource( $config );
+	$shopify_datasource            = new ShopifyDatasource( $access_token, $store_name );
 	$shopify_search_products_query = new ShopifySearchProductsQuery( $shopify_datasource );
 	$shopify_get_product_query     = new ShopifyGetProductQuery( $shopify_datasource );
 
@@ -76,4 +43,8 @@ function register_blocks_for_shopify_data_source( array $config, string $block_p
 	ConfigurationLoader::register_query( $block_name, new ShopifyRemoveFromCartMutation( $shopify_datasource ) );
 
 	ConfigurationLoader::register_block_pattern( $block_name, 'remote-data-blocks/shopify-product-teaser', $block_pattern, [ 'title' => 'Shopify Product Teaser' ] );
+
+	register_block_type( __DIR__ . '/build/blocks/shopify-cart' );
+	register_block_type( __DIR__ . '/build/blocks/shopify-cart-button' );
 }
+add_action( 'register_remote_data_blocks', __NAMESPACE__ . '\\register_shopify_block' );
