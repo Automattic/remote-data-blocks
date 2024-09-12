1aa0d7a1b368ce1ac11afad675a50e4c5921963d
Hew <github@mhs.fastmail.com>
2024-09-12 14:47:14 -0400

    meh

--- inc/config/query-context/query-context-interface.php
+++ inc/config/query-context/query-context-interface.php
@@ -12,10 +12,11 @@ namespace RemoteDataBlocks\Config;
 use Psr\Http\Message\ResponseInterface;
 
 interface QueryContextInterface {
+	public function __construct( DatasourceInterface $datasource );
 	public function get_image_url(): string|null;
 	public function get_metadata( ResponseInterface $response, array $query_results ): array;
 	public function get_query_name(): string;
 	public function get_query_runner(): QueryRunnerInterface;
 	public function is_response_data_collection(): bool;
-    public function process_response( string $raw_response_data, array $input_variables ): string|array|object|null;
+	public function process_response( string $raw_response_data, array $input_variables ): string|array|object|null;
 }
