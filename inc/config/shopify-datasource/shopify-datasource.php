f31bde5e92fed6f31589f80fd74c3800e281b0c2
Jeff Bowen <jblz@users.noreply.github.com>
2024-09-13 11:15:14 -0400

    cleanup

--- inc/config/shopify-datasource/shopify-datasource.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once __DIR__ . '/class-shopify-datasource.php';
-require_once __DIR__ . '/class-shopify-get-product-query.php';
-require_once __DIR__ . '/class-shopify-search-products-query.php';
