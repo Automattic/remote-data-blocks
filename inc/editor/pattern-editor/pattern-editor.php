030006e3546590c1de14c530fee594e605e61505
Chris Zarate <chris@zarate.org>
2024-09-14 16:47:13 -0400

    Fix category registration and meta registration (#51)
    * Use categories consistently across blocks and patterns
    
    * Move register_meta to init hook

--- inc/editor/pattern-editor/pattern-editor.php
+++ inc/editor/pattern-editor/pattern-editor.php
@@ -5,25 +5,33 @@ namespace RemoteDataBlocks\Editor;
 defined( 'ABSPATH' ) || exit();
 
 class PatternEditor {
-	public static function init() {
-		register_post_meta( 'wp_block', '_remote_data_blocks_block_type', [
-			'show_in_rest' => true,
-			'single'       => true,
-			'type'         => 'string',
-		] );
+	public static $block_type_meta_key = '_remote_data_blocks_block_type';
 
-		add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\\PatternEditor::enqueue_block_editor_assets' );
+	public static function init() {
+		add_action( 'init', [ __CLASS__, 'register_meta' ], 10, 0 );
+		add_action( 'enqueue_block_editor_assets', [ __CLASS__, 'enqueue_block_editor_assets' ], 10, 0 );
+	}
+
+	public static function register_meta() {
+		register_post_meta( 'wp_block', self::$block_type_meta_key, [
+			'auth_callback' => function ( bool $_allowed, string $meta_key, int $object_id ) {
+				return current_user_can( 'edit_post_meta', $object_id );
+			},
+			'show_in_rest'  => true,
+			'single'        => true,
+			'type'          => 'string',
+		] );
 	}
 
 	public static function enqueue_block_editor_assets() {
 		$asset_file = REMOTE_DATA_BLOCKS__PLUGIN_DIRECTORY . '/build/pattern-editor/index.asset.php';
-	
+
 		if ( ! file_exists( $asset_file ) ) {
 			wp_die( 'The settings asset file is missing. Run `npm run build` to generate it.' );
 		}
-	
+
 		$asset = include $asset_file;
-	
+
 		wp_enqueue_script(
 			'remote-data-blocks-pattern-editor',
 			plugins_url( 'build/pattern-editor/index.js', REMOTE_DATA_BLOCKS__PLUGIN_ROOT ),
