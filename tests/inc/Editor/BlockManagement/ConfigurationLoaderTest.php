a54c61434dc1ca520aeb8e2ff98bd4e31215949a
Hew <github@mhs.fastmail.com>
2024-09-15 21:48:49 -0400

    This too

--- tests/inc/Editor/BlockManagement/ConfigurationLoaderTest.php
+++ tests/inc/Editor/BlockManagement/ConfigurationLoaderTest.php
@@ -5,7 +5,7 @@ namespace RemoteDataBlocks\Tests\Editor\BlockManagement;
 use PHPUnit\Framework\TestCase;
 use RemoteDataBlocks\Config\QueryContext\HttpQueryContext;
 use RemoteDataBlocks\Editor\BlockManagement\ConfigurationLoader;
-use RemoteDataBlocks\Tests\TestDatasource;
+use RemoteDataBlocks\Tests\Mocks\MockDatasource;
 
 class ConfigurationLoaderTest extends TestCase {
 	public function testIsRegisteredBlockReturnsFalseWhenNoConfigurations() {
@@ -13,7 +13,7 @@ class ConfigurationLoaderTest extends TestCase {
 	}
 
 	public function testIsRegisteredBlockReturnsTrueForRegisteredBlock() {
-		ConfigurationLoader::register_block( 'some_slick_block', new HttpQueryContext( new TestDatasource() ) );
+		ConfigurationLoader::register_block( 'some_slick_block', new HttpQueryContext( new MockDatasource() ) );
 		$this->assertTrue( ConfigurationLoader::is_registered_block( 'remote-data-blocks/some_slick_block' ) );
 	}
 }
