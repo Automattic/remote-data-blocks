b8ff04cc08d2e2803508d6cbb6fc24f60c3748b6
Hew <github@mhs.fastmail.com>
2024-09-15 21:47:24 -0400

    tweak

--- tests/inc/bootstrap.php
+++ tests/inc/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 define( 'ABSPATH', __FILE__ );
-define( 'REMOTE_DATA_BLOCKS__PLUGIN_DIRECTORY', __DIR__ . '/../' );
+define( 'REMOTE_DATA_BLOCKS__PLUGIN_DIRECTORY', __DIR__ . '/../../' );
 define( 'REMOTE_DATA_BLOCKS__UNIT_TEST', true );
 
 require_once __DIR__ . '/../../vendor/autoload.php';
