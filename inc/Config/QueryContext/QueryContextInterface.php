cfb91d2810b7f13137f51538c0c94ce371afead5
Hew <2006764+mhsdef@users.noreply.github.com>
2024-09-25 12:22:51 -0400

    Fix misc bugs, h/t Psalm (#103)
    * Fix misc bugs, h/t Psalm
    
    * ayo
    
    * mmm
    
    * oh
    
    * bump
    
    * ohh
    
    * tweak
    
    * mmoo
    
    * Add back some wordles
    
    * urgh
    
    * put back uses
    
    * these too
    
    * Adjust
    
    * tweak
    
    * just revert it for now
    
    * just do this for now

--- inc/Config/QueryContext/QueryContextInterface.php
+++ inc/Config/QueryContext/QueryContextInterface.php
@@ -17,5 +17,5 @@ interface QueryContextInterface {
 	public function get_query_name(): string;
 	public function get_query_runner(): QueryRunnerInterface;
 	public function is_response_data_collection(): bool;
-	public function process_response( string $raw_response_string, array $input_variables ): string|array|object|null;
+	public function process_response( string $raw_response_data, array $input_variables ): string|array|object|null;
 }
