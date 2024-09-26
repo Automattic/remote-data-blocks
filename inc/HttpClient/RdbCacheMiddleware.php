9b30703ba1f2fece3c32fd6d7b5eb8e663a6f9ee
Hew <github@mhs.fastmail.com>
2024-09-26 12:14:56 -0400

    now we can take this out too apparently

--- inc/HttpClient/RdbCacheMiddleware.php
+++ inc/HttpClient/RdbCacheMiddleware.php
@@ -5,8 +5,6 @@ namespace RemoteDataBlocks\HttpClient;
 class RdbCacheMiddleware extends \Kevinrob\GuzzleCache\CacheMiddleware {
 	/**
 	 * @var array<string, true>
-	 *
-	 * @psalm-suppress NonInvariantPropertyType reason: we are right per Kevinrob's comment in parent
 	 */
 	// phpcs:ignore WordPress.NamingConventions.ValidVariableName.PropertyNotSnakeCase, SlevomatCodingStandard.TypeHints.PropertyTypeHint.MissingNativeTypeHint
 	protected $httpMethods = [
