2b36b75bdcd2bfc7ed1319e13d50b33e591aada6
Hew <github@mhs.fastmail.com>
2024-09-27 15:52:14 -0400

    mmmm

--- inc/Integrations/Airtable/AirtableGetItemQuery.php
+++ inc/Integrations/Airtable/AirtableGetItemQuery.php
@@ -5,24 +5,6 @@ namespace RemoteDataBlocks\Integrations\Airtable;
 use RemoteDataBlocks\Config\QueryContext\HttpQueryContext;
 
 class AirtableGetItemQuery extends HttpQueryContext {
-	public function get_input_schema(): array {
-		return [
-			'record_id' => [
-				'name'      => 'Record ID',
-				'overrides' => [
-					[
-						'target' => 'utm_content',
-						'type'   => 'query_var',
-					],
-				],
-				'type'      => 'id',
-			],
-		];
-	}
-
-	/**
-	 * Airtable API endpoint for fetching a single item.
-	 */
 	public function get_endpoint( array $input_variables ): string {
 		$data_source_config = $this->get_datasource()->to_array();
 		return $this->get_datasource()->get_endpoint() . '/' . $data_source_config['tables'][0]['id'] . '/' . $input_variables['record_id'];
--- inc/Integrations/Airtable/AirtableListItemsQuery.php
+++ inc/Integrations/Airtable/AirtableListItemsQuery.php
@@ -5,9 +5,6 @@ namespace RemoteDataBlocks\Integrations\Airtable;
 use RemoteDataBlocks\Config\QueryContext\HttpQueryContext;
 
 class AirtableListItemsQuery extends HttpQueryContext {
-	/**
-	 * Airtable API endpoint for listing items.
-	 */
 	public function get_endpoint( array $input_variables ): string {
 		$data_source_config = $this->get_datasource()->to_array();
 		return $this->get_datasource()->get_endpoint() . '/' . $data_source_config['tables'][0]['id'];
