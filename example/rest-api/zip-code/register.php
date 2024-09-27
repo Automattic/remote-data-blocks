3774d6b3e38a11cf214ad5de69dadcdf15dda698
Hew <2006764+mhsdef@users.noreply.github.com>
2024-09-27 12:33:54 -0400

    Move Zip Code integration to generic HTTP integration (#122)
    * Move Zip Code integration to generic HTTP integration
    
    * Woop
    
    * lint

--- example/rest-api/zip-code/register.php
+++ example/rest-api/zip-code/register.php
@@ -2,15 +2,19 @@
 
 namespace RemoteDataBlocks\Example\ZipCode;
 
-require_once __DIR__ . '/inc/queries/class-zip-code-datasource.php';
+use RemoteDataBlocks\Integrations\GenericHttp\GenericHttpDatasource;
+use RemoteDataBlocks\Logging\LoggerManager;
+
 require_once __DIR__ . '/inc/queries/class-get-zip-code-query.php';
 
 function register_zipcode_block() {
-	$zipcode_datasource = ZipCodeDatasource::from_array( [
-		'slug'    => 'zip-code',
-		'service' => 'zip-code',
-	] );
-	
+	$zipcode_datasource = GenericHttpDatasource::from_slug( 'zip-code' );
+
+	if ( ! $zipcode_datasource instanceof GenericHttpDatasource ) {
+		LoggerManager::instance()->debug( 'Zip Code datasource not found' );
+		return;
+	}
+
 	$zipcode_query = new GetZipCodeQuery( $zipcode_datasource );
 
 	register_remote_data_block( 'Zip Code', $zipcode_query );
