24fecd564893644b4087d68f7f6e34b0c331f426
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2024-10-28 23:37:46 +0500

    💫 UPDATE: sendTracksEvent to not track on local

--- src/blocks/remote-data-container/utils/tracks.ts
+++ src/blocks/remote-data-container/utils/tracks.ts
@@ -1,8 +1,14 @@
 import { recordTracksEvent } from '@automattic/calypso-analytics';
 
 export function sendTracksEvent( eventName: string, props: object ): void {
+	const baseProps = window.wpvipTracksBaseProps;
+
 	// Do not track if the base props are not available i.e. user is not on VIP platform.
-	if ( ! window.wpvipTracksBaseProps ) {
+	if ( ! baseProps ) {
+		return;
+	}
+
+	if ( baseProps.vipgo_env === 'local' ) {
 		return;
 	}
 
--- types/globals.d.ts
+++ types/globals.d.ts
@@ -2,7 +2,7 @@ declare global {
 	var REMOTE_DATA_BLOCKS: LocalizedBlockData | undefined;
 	var REMOTE_DATA_BLOCKS_SETTINGS: LocalizedSettingsData | undefined;
 	var wpvipTracksBaseProps: {
-		vipgo_env: number;
+		vipgo_env: string;
 		vipgo_org: number;
 		is_vip_user: boolean;
 		_ui: string; // User ID
