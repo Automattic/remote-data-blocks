1b786c4a81f5f3f20509cbc4e9ba1664e3ce688d
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2024-11-01 14:20:34 +0500

    Add tests for tracks.ts

---
+++ src/blocks/remote-data-container/utils/tracks.spec.ts
@@ -0,0 +1,60 @@
+import { recordTracksEvent } from '@automattic/calypso-analytics';
+import { describe, expect, it, vi, beforeEach } from 'vitest';
+
+import { sendTracksEvent } from '@/blocks/remote-data-container/utils/tracks';
+import { getTracksBaseProps } from '@/utils/localized-block-data';
+import { VipTracksBaseProps } from 'types/globals';
+
+vi.mock( '@automattic/calypso-analytics', () => ( {
+	recordTracksEvent: vi.fn(),
+} ) );
+
+vi.mock( '@/utils/localized-block-data', () => ( {
+	getTracksBaseProps: vi.fn(),
+} ) );
+
+describe( 'sendTracksEvent', () => {
+	const defaultTrackProps: VipTracksBaseProps = {
+		vipgo_env: 'local',
+		vipgo_org: 1,
+		is_vip_user: false,
+		_ui: '1', // User ID
+		_ut: 'anon', // User Type
+	};
+
+	beforeEach( () => {
+		window.VIP_TRACKS_BASE_PROPS = defaultTrackProps;
+		vi.clearAllMocks();
+	} );
+
+	it( 'should not record event if VIP_TRACKS_BASE_PROPS is not defined', () => {
+		window.VIP_TRACKS_BASE_PROPS = undefined;
+
+		sendTracksEvent( 'test_event', { key: 'value' } );
+
+		expect( recordTracksEvent ).not.toHaveBeenCalled();
+	} );
+
+	it( 'should not track if vipgo_env is local', () => {
+		sendTracksEvent( 'test_event', { key: 'value' } );
+
+		expect( recordTracksEvent ).not.toHaveBeenCalled();
+	} );
+
+	it( 'should call recordTracksEvent with the correct event name and merged properties', () => {
+		if ( window.VIP_TRACKS_BASE_PROPS ) {
+			window.VIP_TRACKS_BASE_PROPS.vipgo_env = 'production';
+		}
+		vi.mocked( getTracksBaseProps ).mockReturnValue( { baseProp: 'baseValue' } );
+
+		sendTracksEvent( 'test_event', { customProp: 'customValue' } );
+
+		expect( recordTracksEvent ).toHaveBeenCalledTimes( 1 );
+		expect( recordTracksEvent ).toHaveBeenCalledWith( 'test_event', {
+			...defaultTrackProps,
+			vipgo_env: 'production',
+			baseProp: 'baseValue',
+			customProp: 'customValue',
+		} );
+	} );
+} );
--- types/globals.d.ts
+++ types/globals.d.ts
@@ -1,15 +1,18 @@
+import { LocalizedBlockData } from './localized-block-data';
+import { LocalizedSettingsData } from './localized-settings';
+
 declare global {
 	var REMOTE_DATA_BLOCKS: LocalizedBlockData | undefined;
 	var REMOTE_DATA_BLOCKS_SETTINGS: LocalizedSettingsData | undefined;
-	var VIP_TRACKS_BASE_PROPS:
-		| {
-				vipgo_env: string;
-				vipgo_org: number;
-				is_vip_user: boolean;
-				_ui: string; // User ID
-				_ut: string; // User Type
-		  }
-		| undefined;
+	var VIP_TRACKS_BASE_PROPS: VipTracksBaseProps | undefined;
+}
+
+export interface VipTracksBaseProps {
+	vipgo_env: string;
+	vipgo_org: number;
+	is_vip_user: boolean;
+	_ui: string; // User ID
+	_ut: string; // User Type
 }
 
 export {};
