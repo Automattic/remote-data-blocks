f64f5452c6116bcc1a3678305d12d200074bff38
brookewp <brooke.kaminski@automattic.com>
2024-11-13 16:24:10 -0800

    improve sanitization
    Signed-off-by: brookewp <brooke.kaminski@automattic.com>

--- src/data-sources/utils.tsx
+++ src/data-sources/utils.tsx
@@ -2,7 +2,14 @@ import CheckIcon from '@/settings/icons/CheckIcon';
 import ErrorIcon from '@/settings/icons/ErrorIcon';
 
 export const sanitizeDataSourceSlug = ( slug: string ) => {
-	return slug.replace( /[^a-z0-9-]/gi, '' ).toLowerCase();
+	return slug
+		.toString() // Ensure input is a string
+		.toLowerCase() // Convert to lowercase
+		.trim() // Trim leading and trailing spaces
+		.replace( /\s+/g, '-' ) // Replace spaces with hyphens
+		.replace( /[^a-z0-9-]/g, '' ) // Remove invalid characters
+		.replace( /--+/g, '-' ) // Replace multiple hyphens with a single hyphen
+		.replace( /^-+|-+$/g, '' ); // Trim leading and trailing hyphens
 };
 
 export function getConnectionMessage(
