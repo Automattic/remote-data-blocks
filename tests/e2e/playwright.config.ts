3fa0827b0aedde6497869df628d90ab6d34a5e49
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2024-11-19 19:58:24 +0500

    IMPROVE: configurations

--- tests/e2e/playwright.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from '@playwright/test';
-
-const baseConfig = require( '@wordpress/scripts/config/playwright.config' );
-
-const config = defineConfig( {
-	...baseConfig,
-} );
-
-export default config;
