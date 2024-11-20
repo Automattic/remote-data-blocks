94422034c16125137f81a1bd822a5165b9c0f19c
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2024-11-20 18:41:56 +0500

    💫 UPDATE: docs

--- tests/e2e/settings/activation.spec.ts
+++ tests/e2e/settings/activation.spec.ts
@@ -3,7 +3,6 @@ import { expect, test } from '@wordpress/e2e-test-utils-playwright';
 test.describe( 'plugin activation', () => {
 	test( 'should have the "Remote Data Blocks" menu item in sidebar', async ( { admin, page } ) => {
 		await admin.visitAdminPage( '/' );
-
 		await page.locator( '#menu-settings' ).click();
 
 		const settingsMenuItem = page.locator(
