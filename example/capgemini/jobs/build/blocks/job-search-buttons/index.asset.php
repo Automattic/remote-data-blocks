37d6a4edea22b60f0890ed84a4cbedd728ebe747
chriszarate <chris.zarate@automattic.com>
2024-11-21 14:25:15 -0500

    Update build artifact

---
+++ example/capgemini/jobs/build/blocks/job-search-buttons/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => '2d796ea75544d00c17c4');
---
+++ example/capgemini/jobs/build/blocks/job-search-results/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => '3b148ee93251851c274a');
