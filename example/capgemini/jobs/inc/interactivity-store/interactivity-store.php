747aa33c028aad2f414451e5e24a4da57acd9306
chriszarate <chris.zarate@automattic.com>
2024-11-21 15:07:46 -0500

    Server-side rendered initial state

--- example/capgemini/jobs/inc/interactivity-store/interactivity-store.php
+++ example/capgemini/jobs/inc/interactivity-store/interactivity-store.php
@@ -7,27 +7,50 @@ defined( 'ABSPATH' ) || exit();
 use RemoteDataBlocks\Editor\DataBinding\BlockBindings;
 use RemoteDataBlocks\Editor\BlockManagement\ConfigStore;
 use RemoteDataBlocks\REST\RemoteDataController;
-use WP_Block;
 
 class InteractivityStore {
-	public static function get_context( WP_Block $block ): array {
-		$product_title = BlockBindings::get_value( [ 'field' => 'title' ], $block );
-		$variant_id = BlockBindings::get_value( [ 'field' => 'variant_id' ], $block );
-
-		return [
-			'title' => $product_title,
-			'variantId' => $variant_id,
-		];
-	}
-
 	public static function get_initial_state(): array {
 		$block_name = ConfigStore::get_block_name( 'Job Search Results' );
 		$rest_url = RemoteDataController::get_url();
 
-		return [
+		$initial_state = [
 			'blockName' => $block_name,
+			'jobs' => [],
+			'page' => 1,
 			'restUrl' => $rest_url,
+			'searchTerms' => get_query_var( 'search' ) ?? '',
+			'size' => 10,
 		];
+
+		// If search terms are set via query var, fetch them in order to server-side
+		// render the jobs.
+		//
+		// TODO:
+		// 1. The query vars need to be formalized and configurable. Should it come from
+		//    overrides? Those are slightly different.
+		// 2. Need to support the other input variables instead of hardcoding.
+		if ( ! empty( $initial_state['searchTerms'] ) ) {
+			$block_context = [
+				'blockName' => $block_name,
+				'queryInput' => [
+					'country_code' => $initial_state['country_code'],
+					'page' => $initial_state['page'],
+					'search' => $initial_state['searchTerms'],
+					'size' => 10,
+				],
+			];
+
+			$raw_results = BlockBindings::execute_query( $block_context, 'interactivity-initial-state' );
+			if ( ! is_wp_error( $raw_results ) ) {
+				$initial_state['jobs'] = array_map( function ( $job ) {
+					return [
+						'title' => $job['result']['title']['value'] ?? '',
+					];
+				}, $raw_results['results'] );
+			}
+		}
+
+		return $initial_state;
 	}
 
 	public static function get_store_name(): string {
--- example/capgemini/jobs/src/data/store.ts
+++ example/capgemini/jobs/src/data/store.ts
@@ -2,6 +2,14 @@ import { store } from '@wordpress/interactivity';
 
 import { PUBLIC_STORE_NAME } from '../config/constants';
 
+interface GenericEvent {
+	key?: string;
+	target: {
+		value?: string;
+	};
+	type: string;
+}
+
 interface Job {
 	id: string;
 	title: string;
@@ -25,12 +33,7 @@ interface ViewStore {
 	state: ViewState;
 }
 
-const initialState: ViewInitialState = {
-	jobs: [],
-	searchTerms: '',
-};
-
-const { state } = store< ViewStore >( PUBLIC_STORE_NAME, {
+const { actions, state } = store< ViewStore >( PUBLIC_STORE_NAME, {
 	actions: {
 		search: async (): Promise< void > => {
 			const response = await window.fetch( state.restUrl, {
@@ -54,18 +57,17 @@ const { state } = store< ViewStore >( PUBLIC_STORE_NAME, {
 					title: result.result?.title?.value ?? 'title',
 				} ) ) ?? [];
 		},
-		updateSearchTerms: ( evt: React.ChangeEvent< HTMLInputElement > ) => {
+		updateSearchTerms: ( evt: GenericEvent ) => {
 			// TODO: Debounce
-			state.searchTerms = evt.target?.value;
+			state.searchTerms = evt.target?.value ?? '';
+
+			if ( evt.type === 'keydown' && evt.key === 'Enter' ) {
+				void actions.search();
+			}
 		},
 	},
 	state: {
-		// It's important to enumerate all properties here instead of spreading
-		// another object, so that they are captured by the object proxy wrapper.
-		// Otherwise you will get an error.
-		jobs: initialState.jobs,
-
-		// We rely on the default values for blockName and restUrl, supplied by
-		// wp_interactivity_state() in render.php.
+		// We rely on the initial state supplied by wp_interactivity_state() in
+		// render.php, which is merged with the default initial state here.
 	},
 } );
