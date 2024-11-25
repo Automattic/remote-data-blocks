a4eeea050d5bd29163df94770797425914237269
Chris Zarate <chris.zarate@automattic.com>
2024-11-25 17:40:51 -0500

    Restore missing block binding controls (#218)

--- src/blocks/remote-data-container/config/constants.ts
+++ src/blocks/remote-data-container/config/constants.ts
@@ -1,6 +1,13 @@
 import { getRestUrl } from '@/utils/localized-block-data';
 import { getClassName } from '@/utils/string';
 
+export const SUPPORTED_CORE_BLOCKS = [
+	'core/button',
+	'core/heading',
+	'core/image',
+	'core/paragraph',
+];
+
 export const DISPLAY_QUERY_KEY = '__DISPLAY__';
 export const REMOTE_DATA_CONTEXT_KEY = 'remote-data-blocks/remoteData';
 export const REMOTE_DATA_REST_API_URL = getRestUrl();
