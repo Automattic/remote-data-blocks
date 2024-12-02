d6876789e97e7fcb3ab8d5de724cb148246c903a
brookewp <brooke.kaminski@automattic.com>
2024-12-02 14:35:41 -0800

    Add UUIDs to examples
    Signed-off-by: brookewp <brooke.kaminski@automattic.com>

--- inc/Integrations/GitHub/GitHubDataSource.php
+++ inc/Integrations/GitHub/GitHubDataSource.php
@@ -3,6 +3,7 @@
 namespace RemoteDataBlocks\Integrations\GitHub;
 
 use RemoteDataBlocks\Config\DataSource\HttpDataSource;
+use function wp_generate_uuid4;
 
 class GitHubDataSource extends HttpDataSource {
 	protected const SERVICE_NAME = REMOTE_DATA_BLOCKS_GITHUB_SERVICE;
@@ -60,13 +61,14 @@ class GitHubDataSource extends HttpDataSource {
 		return $this->config['ref'];
 	}
 	
-	public static function create( string $repo_owner, string $repo_name, string $ref ): self {
+	public static function create( string $repo_owner, string $repo_name, string $ref, ?string $uuid ): self {
 		return parent::from_array([
             'display_name' => sprintf( 'GitHub: %s/%s (%s)', $repo_owner, $repo_name, $ref ),
 			'service' => REMOTE_DATA_BLOCKS_GITHUB_SERVICE,
 			'repo_owner' => $repo_owner,
 			'repo_name' => $repo_name,
 			'ref' => $ref,
+            'uuid' => $uuid ?? wp_generate_uuid4(),
 		]);
 	}
 }
