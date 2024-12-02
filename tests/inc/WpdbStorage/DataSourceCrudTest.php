384f13ad38c5c07a1d2af970032be8a1310ac811
brookewp <brooke.kaminski@automattic.com>
2024-12-02 14:11:07 -0800

    Update tests
    Signed-off-by: brookewp <brooke.kaminski@automattic.com>

--- tests/inc/WpdbStorage/DataSourceCrudTest.php
+++ tests/inc/WpdbStorage/DataSourceCrudTest.php
@@ -11,16 +11,6 @@ class DataSourceCrudTest extends TestCase {
 	protected function tearDown(): void {
 		clear_mocked_options();
 	}
-
-
-	public function test_validate_slug_with_valid_input() {
-		$this->assertTrue( DataSourceCrud::validate_slug( 'valid-slug' ) );
-	}
-
-	public function test_validate_slug_with_invalid_input() {
-		$this->assertInstanceOf( WP_Error::class, DataSourceCrud::validate_slug( '' ) );
-		$this->assertInstanceOf( WP_Error::class, DataSourceCrud::validate_slug( 'INVALID_SLUG' ) );
-	}
 	
 	public function test_register_new_data_source_with_valid_input() {
 		$valid_source = [
@@ -34,7 +24,6 @@ class DataSourceCrudTest extends TestCase {
 			],
 			'tables' => [],
 			'display_name' => 'Crud Test',
-			'slug' => 'valid-slug',
 		];
 
 		$result = DataSourceCrud::register_new_data_source( $valid_source );
@@ -76,7 +65,6 @@ class DataSourceCrudTest extends TestCase {
 			],
 			'tables' => [],
 			'display_name' => 'Base Name 1',
-			'slug' => 'source-1',
 		] );
 
 		$source2 = DataSourceCrud::register_new_data_source( [
@@ -85,7 +73,6 @@ class DataSourceCrudTest extends TestCase {
 			'uuid' => wp_generate_uuid4(),
 			'access_token' => 'token2',
 			'store_name' => 'mystore',
-			'slug' => 'source-2',
 		] );
 
 		set_mocked_option( DataSourceCrud::CONFIG_OPTION_NAME, [
@@ -98,11 +85,11 @@ class DataSourceCrudTest extends TestCase {
 
 		$airtable_sources = DataSourceCrud::get_data_sources( 'airtable' );
 		$this->assertCount( 1, $airtable_sources );
-		$this->assertSame( 'source-1', $airtable_sources[0]['slug'] );
+		$this->assertSame( 'Base Name 1', $airtable_sources[0]['display_name'] );
 
 		$shopify_sources = DataSourceCrud::get_data_sources( 'shopify' );
 		$this->assertCount( 1, $shopify_sources );
-		$this->assertSame( 'source-2', $shopify_sources[0]['slug'] );
+		$this->assertSame( 'mystore', $shopify_sources[0]['store_name'] );
 	}
 
 	public function test_get_item_by_uuid_with_valid_uuid() {
@@ -117,7 +104,6 @@ class DataSourceCrudTest extends TestCase {
 			],
 			'tables' => [],
 			'display_name' => 'Crud Test',
-			'slug' => 'source-1',
 		] );
 
 		$retrieved_source = DataSourceCrud::get_item_by_uuid( DataSourceCrud::get_data_sources(), $source->to_array()['uuid'] );
@@ -140,6 +126,7 @@ class DataSourceCrudTest extends TestCase {
 			'service' => REMOTE_DATA_BLOCKS_AIRTABLE_SERVICE,
 			'service_schema_version' => 1,
 			'uuid' => wp_generate_uuid4(),
+            'newUUID' => '2ced7a88-5a61-4e66-840e-26baefdfdfd5',
 			'access_token' => 'token1',
 			'base' => [
 				'id' => 'base_id1',
@@ -147,17 +134,16 @@ class DataSourceCrudTest extends TestCase {
 			],
 			'tables' => [],
 			'display_name' => 'Crud Test',
-			'slug' => 'source-1',
 		] );
 
 		$updated_source = DataSourceCrud::update_item_by_uuid( $source->to_array()['uuid'], [
 			'access_token' => 'updated_token',
-			'slug' => 'updated-slug',
+			'uuid' => '2ced7a88-5a61-4e66-840e-26baefdfdfd5',
 		] );
 
 		$this->assertInstanceOf( HttpDataSource::class, $updated_source );
 		$this->assertSame( 'updated_token', $updated_source->to_array()['access_token'] );
-		$this->assertSame( 'updated-slug', $updated_source->to_array()['slug'] );
+		$this->assertSame( '2ced7a88-5a61-4e66-840e-26baefdfdfd5', $updated_source->to_array()['uuid'] );
 	}
 
 	public function test_update_item_by_uuid_with_invalid_uuid() {
@@ -177,7 +163,6 @@ class DataSourceCrudTest extends TestCase {
 			],
 			'tables' => [],
 			'display_name' => 'Crud Test',
-			'slug' => 'source-1',
 		] );
 
 		$result = DataSourceCrud::delete_item_by_uuid( $source->to_array()['uuid'] );
@@ -187,30 +172,8 @@ class DataSourceCrudTest extends TestCase {
 		$this->assertFalse( $deleted_source );
 	}
 
-	public function test_get_by_slug_with_existing_slug() {
-		DataSourceCrud::register_new_data_source([
-			'service' => REMOTE_DATA_BLOCKS_AIRTABLE_SERVICE,
-			'service_schema_version' => 1,
-			'uuid' => wp_generate_uuid4(),
-			'access_token' => 'token1',
-			'base' => [
-				'id' => 'base_id1',
-				'name' => 'Base Name 1',
-			],
-			'tables' => [],
-			'display_name' => 'Crud Test',
-			'slug' => 'existing-slug',
-		]);
-
-		$source = DataSourceCrud::get_by_slug( 'existing-slug' );
-		$this->assertIsArray( $source );
-		$this->assertSame( 'existing-slug', $source['slug'] );
-		$this->assertSame( 'token1', $source['access_token'] );
-		$this->assertSame( 'base_id1', $source['base']['id'] );
-	}
-
-	public function test_get_by_slug_with_non_existent_slug() {
-		$non_existent = DataSourceCrud::get_by_slug( 'non-existent-slug' );
+	public function test_get_by_uuid_with_non_existent_uuid() {
+		$non_existent = DataSourceCrud::get_by_uuid( '64af9297-867e-4e39-b51d-7c97beeebec6' );
 		$this->assertFalse( $non_existent );
 	}
 }
