ecb7ce4b4817ae63819b2f245d5c4917caeecdbf
brookewp <brooke.kaminski@automattic.com>
2024-12-04 19:02:39 -0800

    Add steps to setup flow
    Signed-off-by: brookewp <brooke.kaminski@automattic.com>

--- src/data-sources/airtable/AirtableSettings.tsx
+++ src/data-sources/airtable/AirtableSettings.tsx
@@ -8,7 +8,6 @@ import { SUPPORTED_AIRTABLE_TYPES } from '@/data-sources/airtable/constants';
 import { AirtableFormState } from '@/data-sources/airtable/types';
 import { getAirtableOutputQueryMappingValue } from '@/data-sources/airtable/utils';
 import { DataSourceForm } from '@/data-sources/components/DataSourceForm';
-import { DataSourceFormActions } from '@/data-sources/components/DataSourceFormActions';
 import PasswordInputControl from '@/data-sources/components/PasswordInputControl';
 import {
 	useAirtableApiBases,
@@ -24,6 +23,7 @@ import {
 import { getConnectionMessage } from '@/data-sources/utils';
 import { useForm } from '@/hooks/useForm';
 import { useSettingsContext } from '@/settings/hooks/useSettingsNav';
+import AirtableIcon from '@/settings/icons/AirtableIcon';
 import { StringIdName } from '@/types/common';
 import { SelectOption } from '@/types/input';
 
@@ -304,85 +304,79 @@ export const AirtableSettings = ( {
 	}, [ state.table, tables ] );
 
 	return (
-		<DataSourceForm
-			displayName={ state.display_name }
-			handleOnChange={ handleOnChange }
-			heading={
-				mode === 'add' ? __( 'Add Airtable Data Source' ) : __( 'Edit Airtable Data Source' )
-			}
-			mode={ mode }
-			newUUID={ newUUID }
-			setNewUUID={ setNewUUID }
-			uuidFromProps={ uuidFromProps }
-		>
-			<div className="form-group">
-				<PasswordInputControl
-					label={ __( 'Access Token', 'remote-data-blocks' ) }
-					onChange={ onTokenInputChange }
-					value={ state.access_token }
-					help={ connectionMessage }
-				/>
-			</div>
+		<>
+			<DataSourceForm icon={ AirtableIcon } mode={ mode } onSave={ onSaveClick } source="Airtable">
+				<DataSourceForm.Setup
+					displayName={ state.display_name }
+					handleOnChange={ handleOnChange }
+					heading={
+						mode === 'add' ? __( 'Add Airtable Data Source' ) : __( 'Edit Airtable Data Source' )
+					}
+					mode={ mode }
+					newUUID={ newUUID }
+					setNewUUID={ setNewUUID }
+					uuidFromProps={ uuidFromProps }
+				>
+					<PasswordInputControl
+						label={ __( 'Access Token', 'remote-data-blocks' ) }
+						onChange={ onTokenInputChange }
+						value={ state.access_token }
+						help={ connectionMessage }
+					/>
+				</DataSourceForm.Setup>
+				<DataSourceForm.Scope>
+					<div className="form-group">
+						<SelectControl
+							id="base"
+							label={ __( 'Base', 'remote-data-blocks' ) }
+							value={ state.base?.id ?? '' }
+							onChange={ onSelectChange }
+							options={ baseOptions }
+							help={ basesHelpText }
+							disabled={ fetchingBases || ! bases?.length }
+							__next40pxDefaultSize
+						/>
+						<SelectControl
+							id="table"
+							label={ __( 'Table', 'remote-data-blocks' ) }
+							value={ state.table?.id ?? '' }
+							onChange={ onSelectChange }
+							options={ tableOptions }
+							help={ tablesHelpText }
+							disabled={ fetchingTables || ! tables?.length }
+							__next40pxDefaultSize
+						/>
+					</div>
 
-			<div className="form-group">
-				<SelectControl
-					id="base"
-					label={ __( 'Base', 'remote-data-blocks' ) }
-					value={ state.base?.id ?? '' }
-					onChange={ onSelectChange }
-					options={ baseOptions }
-					help={ basesHelpText }
-					disabled={ fetchingBases || ! bases?.length }
-					__next40pxDefaultSize
-				/>
-			</div>
-
-			<div className="form-group">
-				<SelectControl
-					id="table"
-					label={ __( 'Table', 'remote-data-blocks' ) }
-					value={ state.table?.id ?? '' }
-					onChange={ onSelectChange }
-					options={ tableOptions }
-					help={ tablesHelpText }
-					disabled={ fetchingTables || ! tables?.length }
-					__next40pxDefaultSize
-				/>
-			</div>
-
-			{ state.table && availableTableFields.length && (
-				<div className="form-group">
-					<BaseControl
-						label={ __( 'Table Fields', 'remote-data-blocks' ) }
-						help={ __(
-							'Select the fields to be used in the remote data block.',
-							'remote-data-blocks'
-						) }
-					>
-						{ availableTableFields.map( field => (
-							<CheckboxControl
-								key={ field }
-								label={ field }
-								checked={ state.table_fields.has( field ) }
-								onChange={ checked =>
-									handleOnChange(
-										'table_fields',
-										checked
-											? new Set( [ ...state.table_fields, field ] )
-											: new Set( [ ...state.table_fields ].filter( fld => fld !== field ) )
-									)
-								}
-							/>
-						) ) }
-					</BaseControl>
-				</div>
-			) }
-
-			<DataSourceFormActions
-				onSave={ onSaveClick }
-				onCancel={ goToMainScreen }
-				isSaveDisabled={ ! shouldAllowSubmit }
-			/>
-		</DataSourceForm>
+					{ state.table && availableTableFields.length && (
+						<div className="form-group">
+							<BaseControl
+								label={ __( 'Table Fields', 'remote-data-blocks' ) }
+								help={ __(
+									'Select the fields to be used in the remote data block.',
+									'remote-data-blocks'
+								) }
+							>
+								{ availableTableFields.map( field => (
+									<CheckboxControl
+										key={ field }
+										label={ field }
+										checked={ state.table_fields.has( field ) }
+										onChange={ checked =>
+											handleOnChange(
+												'table_fields',
+												checked
+													? new Set( [ ...state.table_fields, field ] )
+													: new Set( [ ...state.table_fields ].filter( fld => fld !== field ) )
+											)
+										}
+									/>
+								) ) }
+							</BaseControl>
+						</div>
+					) }
+				</DataSourceForm.Scope>
+			</DataSourceForm>
+		</>
 	);
 };
