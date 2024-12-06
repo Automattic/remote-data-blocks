bba9d999dcb8548996721db8af9b8ccf25db58b1
chriszarate <chris.zarate@automattic.com>
2024-12-06 00:03:54 -0500

    Update GoogleSheetsDataSource

--- inc/Integrations/Google/Sheets/GoogleSheetsDataSource.php
+++ inc/Integrations/Google/Sheets/GoogleSheetsDataSource.php
@@ -5,118 +5,83 @@ namespace RemoteDataBlocks\Integrations\Google\Sheets;
 use RemoteDataBlocks\Config\DataSource\HttpDataSource;
 use RemoteDataBlocks\Integrations\Google\Auth\GoogleAuth;
 use RemoteDataBlocks\Validation\Types;
+use RemoteDataBlocks\Validation\Validator;
 use WP_Error;
 
 class GoogleSheetsDataSource extends HttpDataSource {
 	protected const SERVICE_NAME = REMOTE_DATA_BLOCKS_GOOGLE_SHEETS_SERVICE;
 	protected const SERVICE_SCHEMA_VERSION = 1;
 
-	protected const SERVICE_SCHEMA = [
-		'type' => 'object',
-		'properties' => [       
-			'credentials' => [
-				'type' => 'object',
-				'properties' => [
-					'type' => [ 'type' => 'string' ],
-					'project_id' => [ 'type' => 'string' ],
-					'private_key_id' => [ 'type' => 'string' ],
-					'private_key' => [
-						'type' => 'string',
-						'sanitize' => false,
-					],
-					'client_email' => [
-						'type' => 'string',
-						'callback' => 'is_email',
-						'sanitize' => 'sanitize_email',
-					],
-					'client_id' => [ 'type' => 'string' ],
-					'auth_uri' => [
-						'type' => 'string',
-						'sanitize' => 'sanitize_url',
-					],
-					'token_uri' => [
-						'type' => 'string',
-						'sanitize' => 'sanitize_url',
-					],
-					'auth_provider_x509_cert_url' => [
-						'type' => 'string',
-						'sanitize' => 'sanitize_url',
-					],
-					'client_x509_cert_url' => [
-						'type' => 'string',
-						'sanitize' => 'sanitize_url',
-					],
-					'universe_domain' => [ 'type' => 'string' ],
-				],
-			],
-			'display_name' => [
-				'type' => 'string',
-				'required' => false,
-			],
-			'spreadsheet' => [
-				'type' => 'object',
-				'properties' => [
-					'name' => [
-						'type' => 'string',
-						'required' => false, // Spreadsheet name is not required to fetch data.
-					],
-					'id' => [ 'type' => 'string' ],
-				],
-			],
-			'sheet' => [
-				'type' => 'object',
-				'properties' => [
-					'name' => [ 'type' => 'string' ],
-					'id' => [ 'type' => 'integer' ],
-				],
-			],
-		],
-	];
+	public static function create( array $service_config, array $config_overrides = [] ): self|WP_Error {
+		$validator = new Validator( self::get_service_config_schema() );
+		$validated = $validator->validate( $service_config );
 
-	public function get_display_name(): string {
-		return sprintf( 'Google Sheets: %s', $this->config['display_name'] );
-	}
+		if ( is_wp_error( $validated ) ) {
+			return $validated;
+		}
 
-	public function get_endpoint(): string {
-		return sprintf( 'https://sheets.googleapis.com/v4/spreadsheets/%s', $this->config['spreadsheet']['id'] );
-	}
+		$display_name = sprintf( 'Google Sheets: %s', $service_config['display_name'] );
 
-	public function get_request_headers(): array {
-		$access_token = GoogleAuth::generate_token_from_service_account_key(
-			$this->config['credentials'],
-			GoogleAuth::GOOGLE_SHEETS_SCOPES
+		return self::from_array(
+			array_merge(
+				[
+					'display_name' => $display_name,
+					'endpoint' => sprintf( 'https://sheets.googleapis.com/v4/spreadsheets/%s', $service_config['spreadsheet']['id'] ),
+					'request_headers' => function () use ( $service_config ): array {
+						$access_token = GoogleAuth::generate_token_from_service_account_key(
+							$service_config['credentials'],
+							GoogleAuth::GOOGLE_SHEETS_SCOPES
+						);
+
+						return [
+							'Authorization' => sprintf( 'Bearer %s', $access_token ),
+							'Content-Type' => 'application/json',
+						];
+					},
+					'service' => REMOTE_DATA_BLOCKS_GITHUB_SERVICE,
+					'service_config' => $service_config,
+					'slug' => sanitize_title( $display_name ),
+				],
+				$config_overrides
+			)
 		);
-
-		return [
-			'Authorization' => sprintf( 'Bearer %s', $access_token ),
-			'Content-Type' => 'application/json',
-		];
 	}
 
-	public static function create( array $credentials, string $spreadsheet_id, string $display_name ): self|WP_Error {
-		return parent::from_array([
-			'service' => REMOTE_DATA_BLOCKS_GOOGLE_SHEETS_SERVICE,
-			'credentials' => $credentials,
-			'display_name' => $display_name,
-			'spreadsheet' => [
-				'name' => '',
-				'id' => $spreadsheet_id,
-			],
-			'sheet' => [
-				'name' => '',
-				'id' => 0,
-			],
-			'slug' => sanitize_title( $display_name ),
-		]);
+	private static function get_service_config_schema(): array {
+		return Types::object( [
+			'credentials' => Types::object( [
+				'type' => Types::string(),
+				'project_id' => Types::string(),
+				'private_key_id' => Types::string(),
+				'private_key' => Types::string( /* TODO sanitize: false? */ ),
+				'client_email' => Types::email_address(),
+				'client_id' => Types::string(),
+				'auth_uri' => Types::url(),
+				'token_uri' => Types::url(),
+				'auth_provider_x509_cert_url' => Types::url(),
+				'client_x509_cert_url' => Types::url(),
+				'universe_domain' => Types::string(),
+			] ),
+			'display_name' => Types::string(),
+			'spreadsheet' => Types::object( [
+				'id' => Types::id(),
+				'name' => Types::nullable( Types::string() ),
+			] ),
+			'sheet' => Types::object( [
+				'name' => Types::string(),
+				'id' => Types::integer(),
+			] ),
+		] );
 	}
 
 	public function to_ui_display(): array {
-		return [
-			'slug' => $this->get_slug(),
-			'service' => REMOTE_DATA_BLOCKS_GOOGLE_SHEETS_SERVICE,
-			'spreadsheet' => [ 'name' => $this->config['spreadsheet_id'] ],
-			'sheet' => [ 'name' => '' ],
-			'uuid' => $this->config['uuid'] ?? null,
-		];
+		return array_merge(
+			parent::to_ui_display(),
+			[
+				'sheet' => [ 'name' => '' ],
+				'spreadsheet' => [ 'name' => $this->config['spreadsheet_id'] ],
+				'uuid' => $this->config['uuid'] ?? null,
+			]
+		);
 	}
 }
