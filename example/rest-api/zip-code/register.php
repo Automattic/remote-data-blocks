fe1794a56e4079083f7bf0f2ebf4af637201d4a7
chriszarate <chris.zarate@automattic.com>
2024-12-08 16:56:36 -0500

    Update Zip code example

--- example/rest-api/zip-code/register.php
+++ example/rest-api/zip-code/register.php
@@ -2,12 +2,11 @@
 
 namespace RemoteDataBlocks\Example\ZipCode;
 
+use RemoteDataBlocks\Config\QueryContext\HttpQueryContext;
 use RemoteDataBlocks\Integrations\GenericHttp\GenericHttpDataSource;
 use RemoteDataBlocks\Logging\LoggerManager;
 
-require_once __DIR__ . '/inc/queries/class-get-zip-code-query.php';
-
-function register_zipcode_block() {
+function register_zipcode_block(): void {
 	$zipcode_data_source = GenericHttpDataSource::from_slug( 'zip-code' );
 
 	if ( ! $zipcode_data_source instanceof GenericHttpDataSource ) {
@@ -15,7 +14,38 @@ function register_zipcode_block() {
 		return;
 	}
 
-	$zipcode_query = new GetZipCodeQuery( $zipcode_data_source );
+	$zipcode_query = HttpQueryContext::from_array( [
+		'data_source' => $zipcode_data_source,
+		'endpoint' => function ( array $input_variables ) use ( $zipcode_data_source ): string {
+			return $zipcode_data_source->get_endpoint() . $input_variables['zip_code'];
+		},
+		'input_schema' => [
+			'zip_code' => [
+				'name' => 'Zip Code',
+				'type' => 'string',
+			],
+		],
+		'output_schema' => [
+			'is_collection' => false,
+			'type' => [
+				'zip_code' => [
+					'name' => 'Zip Code',
+					'path' => '$["post code"]',
+					'type' => 'string',
+				],
+				'city' => [
+					'name' => 'City',
+					'path' => '$.places[0]["place name"]',
+					'type' => 'string',
+				],
+				'state' => [
+					'name' => 'State',
+					'path' => '$.places[0].state',
+					'type' => 'string',
+				],
+			],
+		],
+	] );
 
 	register_remote_data_block( 'Zip Code', $zipcode_query );
 }
