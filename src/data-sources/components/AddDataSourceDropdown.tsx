aa9a98d9e7824c4e986cbcfee8eb8078e111a12f
Max Schmeling <max.schmeling@automattic.com>
2024-12-09 10:37:43 -0600

    Add <SalesforceCommerceB2CIcon>

--- src/data-sources/components/AddDataSourceDropdown.tsx
+++ src/data-sources/components/AddDataSourceDropdown.tsx
@@ -5,6 +5,7 @@ import { chevronDown } from '@wordpress/icons';
 import { SUPPORTED_SERVICES_LABELS } from '../constants';
 import { useSettingsContext } from '@/settings/hooks/useSettingsNav';
 import AirtableIcon from '@/settings/icons/AirtableIcon';
+import SalesforceCommerceB2CIcon from '@/settings/icons/SalesforceCommerceB2CIcon';
 import GoogleSheetsIcon from '@/settings/icons/GoogleSheetsIcon';
 import HttpIcon from '@/settings/icons/HttpIcon';
 import ShopifyIcon from '@/settings/icons/ShopifyIcon';
@@ -51,7 +52,7 @@ export const AddDataSourceDropdown = () => {
 							value: 'shopify',
 						},
 						{
-							icon: HttpIcon,
+							icon: SalesforceCommerceB2CIcon,
 							label: SUPPORTED_SERVICES_LABELS[ 'salesforce-b2c' ],
 							value: 'salesforce-b2c',
 						},
