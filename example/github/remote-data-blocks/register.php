ee3453e7bc4d9464a6e09696fe0440b1a713ae61
chriszarate <chris.zarate@automattic.com>
2024-12-11 13:51:39 -0500

    EXAMPLES: Update GitHub example to use new interfaces

--- example/github/remote-data-blocks/register.php
+++ example/github/remote-data-blocks/register.php
@@ -2,34 +2,128 @@
 
 namespace RemoteDataBlocks\Example\GitHub;
 
+use RemoteDataBlocks\Config\Query\HttpQuery;
 use RemoteDataBlocks\Integrations\GitHub\GitHubDataSource;
-use RemoteDataBlocks\Logging\LoggerManager;
 
-require_once __DIR__ . '/inc/queries/class-github-get-file-as-html-query.php';
-require_once __DIR__ . '/inc/queries/class-github-list-files-query.php';
+require_once __DIR__ . '/github-query-runner.php';
 
-function register_github_file_as_html_block() {
-	$repo_owner = 'Automattic';
-	$repo_name = 'remote-data-blocks';
-	$branch = 'trunk';
+function register_github_file_as_html_block(): void {
+	$service_config = [
+		'__version' => 1,
+		'display_name' => 'Automattic/remote-data-blocks#trunk',
+		'ref' => 'trunk',
+		'repo_owner' => 'Automattic',
+		'repo_name' => 'remote-data-blocks',
+		'request_headers' => [
+			'Accept' => 'application/vnd.github.html+json',
+		],
+	];
 
-	$block_name = sprintf( 'GitHub File As HTML (%s/%s)', $repo_owner, $repo_name );
+	$block_title = sprintf( 'GitHub File As HTML (%s/%s)', $service_config['repo_owner'], $service_config['repo_name'] );
+	$file_extension = '.md';
+	$github_data_source = GitHubDataSource::from_array( [ 'service_config' => $service_config ] );
 
-	$github_data_source = GitHubDataSource::create( $repo_owner, $repo_name, $branch, '37fb2ac2-2399-4095-94d4-d58037d66c61' );
-	$github_get_file_as_html_query = new GitHubGetFileAsHtmlQuery( $github_data_source, '.md' );
-	$github_get_list_files_query = new GitHubListFilesQuery( $github_data_source, '.md' );
-
-	register_remote_data_block( $block_name, $github_get_file_as_html_query );
-	register_remote_data_list_query( $block_name, $github_get_list_files_query );
-
-	$block_pattern = file_get_contents( __DIR__ . '/inc/patterns/file-render.html' );
-	register_remote_data_block_pattern( $block_name, 'GitHub File Render', $block_pattern, [
-		'role' => 'inner_blocks',
+	$github_get_file_as_html_query = HttpQuery::from_array( [
+		'data_source' => $github_data_source,
+		'endpoint' => function ( array $input_variables ) use ( $service_config ): string {
+			return sprintf(
+				'https://api.github.com/repos/%s/%s/contents/%s?ref=%s',
+				$service_config['repo_owner'],
+				$service_config['repo_name'],
+				$input_variables['file_path'],
+				$service_config['ref']
+			);
+		},
+		'input_schema' => [
+			'file_path' => [
+				'name' => 'File Path',
+				'type' => 'string',
+			],
+		],
+		'output_schema' => [
+			'is_collection' => false,
+			'type' => [
+				'file_content' => [
+					'name' => 'File Content',
+					'generate' => [ GitHubQueryRunner::class, 'generate_file_content' ],
+					'type' => 'html',
+				],
+				'file_path' => [
+					'name' => 'File Path',
+					'path' => '$.path',
+					'type' => 'string',
+				],
+			],
+		],
+		'query_runner' => new GitHubQueryRunner(),
 	] );
-	register_remote_data_page( $block_name, 'gh', [ 'allow_nested_paths' => true ] );
 
-	$logger = LoggerManager::instance();
-	$logger->info( sprintf( 'Registered %s block (branch: %s)', $block_name, $branch ) );
+	$github_get_list_files_query = HttpQuery::from_array( [
+		'data_source' => $github_data_source,
+		'input_schema' => [
+			'file_extension' => [
+				'name' => 'File Extension',
+				'type' => 'string',
+			],
+		],
+		'output_schema' => [
+			'is_collection' => true,
+			'path' => sprintf( '$.tree[?(@.path =~ /\\.%s$/)]', ltrim( $file_extension, '.' ) ),
+			'type' => [
+				'file_path' => [
+					'name' => 'File Path',
+					'path' => '$.path',
+					'type' => 'string',
+				],
+				'sha' => [
+					'name' => 'SHA',
+					'path' => '$.sha',
+					'type' => 'string',
+				],
+				'size' => [
+					'name' => 'Size',
+					'path' => '$.size',
+					'type' => 'integer',
+				],
+				'url' => [
+					'name' => 'URL',
+					'path' => '$.url',
+					'type' => 'string',
+				],
+			],
+		],
+	] );
+
+	register_remote_data_block( [
+		'title' => $block_title,
+		'queries' => [
+			'display' => $github_get_file_as_html_query,
+			'list' => $github_get_list_files_query,
+		],
+		'query_input_overrides' => [
+			[
+				'query' => 'display',
+				'source' => 'file_path',
+				'source_type' => 'page',
+				'target' => 'file_path',
+				'target_type' => 'input_var',
+			],
+		],
+		'pages' => [
+			[
+				'allow_nested_paths' => true,
+				'slug' => 'gh',
+				'title' => 'GitHub File',
+			],
+		],
+		'patterns' => [
+			[
+				'html' => file_get_contents( __DIR__ . '/inc/patterns/file-render.html' ),
+				'role' => 'inner_blocks',
+				'title' => 'GitHub File Render',
+			],
+		],
+	] );
 }
 
 add_action( 'init', __NAMESPACE__ . '\\register_github_file_as_html_block' );
