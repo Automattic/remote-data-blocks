6116b73201571f9ab06a334cfe68dd99f0eb48c5
chriszarate <chris.zarate@automattic.com>
2024-12-11 13:17:10 -0500

    SCHEMA: Update primitive types used in blocks

--- src/blocks/remote-data-container/config/constants.ts
+++ src/blocks/remote-data-container/config/constants.ts
@@ -8,12 +8,12 @@ export const SUPPORTED_CORE_BLOCKS = [
 	'core/paragraph',
 ];
 
-export const DISPLAY_QUERY_KEY = '__DISPLAY__';
+export const DISPLAY_QUERY_KEY = 'display';
 export const REMOTE_DATA_CONTEXT_KEY = 'remote-data-blocks/remoteData';
 export const REMOTE_DATA_REST_API_URL = getRestUrl();
 
 export const CONTAINER_CLASS_NAME = getClassName( 'container' );
 
 export const IMAGE_FIELD_TYPES = [ 'image_alt', 'image_url' ];
-export const TEXT_FIELD_TYPES = [ 'number', 'base64', 'currency', 'string' ];
+export const TEXT_FIELD_TYPES = [ 'email_address', 'integer', 'number', 'string' ];
 export const BUTTON_FIELD_TYPES = [ 'button_url' ];
