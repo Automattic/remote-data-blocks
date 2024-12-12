14cc2e2d69b50a581229457d03a40ea225e14a18
chriszarate <chris.zarate@automattic.com>
2024-12-12 13:05:45 -0500

    BLOCK CONFIG: Update ConfigStore to use new block config

--- inc/Editor/BlockManagement/ConfigStore.php
+++ inc/Editor/BlockManagement/ConfigStore.php
@@ -4,10 +4,9 @@ namespace RemoteDataBlocks\Editor\BlockManagement;
 
 defined( 'ABSPATH' ) || exit();
 
+use RemoteDataBlocks\Config\Query\QueryInterface;
 use RemoteDataBlocks\Logging\LoggerManager;
 use Psr\Log\LoggerInterface;
-use RemoteDataBlocks\Config\QueryContext\HttpQueryContext;
-use RemoteDataBlocks\Config\UiDisplayableInterface;
 
 use function sanitize_title;
 
@@ -15,11 +14,12 @@ class ConfigStore {
 	/**
 	 * @var array<string, array<string, mixed>>
 	 */
-	private static array $configurations;
+	private static array $blocks = [];
+
 	private static LoggerInterface $logger;
 
 	public static function init( ?LoggerInterface $logger = null ): void {
-		self::$configurations = [];
+		self::$blocks = [];
 		self::$logger = $logger ?? LoggerManager::instance();
 	}
 
@@ -33,39 +33,37 @@ class ConfigStore {
 	}
 
 	/**
-	 * Get all registered block names.
-	 *
-	 * @return string[]
+	 * Get the configuration for a block.
 	 */
-	public static function get_block_names(): array {
-		return array_keys( self::$configurations );
+	public static function get_block_configurations(): array {
+		return self::$blocks;
 	}
 
 	/**
 	 * Get the configuration for a block.
 	 */
-	public static function get_configuration( string $block_name ): ?array {
+	public static function get_block_configuration( string $block_name ): ?array {
 		if ( ! self::is_registered_block( $block_name ) ) {
 			self::$logger->error( sprintf( 'Block %s has not been registered', $block_name ) );
 			return null;
 		}
 
-		return self::$configurations[ $block_name ];
+		return self::$blocks[ $block_name ];
 	}
 
 	/**
 	 * Set or update the configuration for a block.
 	 */
-	public static function set_configuration( string $block_name, array $config ): void {
+	public static function set_block_configuration( string $block_name, array $config ): void {
 		// @TODO: Validate config shape.
-		self::$configurations[ $block_name ] = $config;
+		self::$blocks[ $block_name ] = $config;
 	}
 
 	/**
 	 * Check if a block is registered.
 	 */
 	public static function is_registered_block( string $block_name ): bool {
-		return isset( self::$configurations[ $block_name ] );
+		return isset( self::$blocks[ $block_name ] );
 	}
 
 	/**
@@ -74,7 +72,7 @@ class ConfigStore {
 	 * @param string $block_name Name of the block.
 	 */
 	public static function get_data_source_type( string $block_name ): ?string {
-		$config = self::get_configuration( $block_name );
+		$config = self::get_block_configuration( $block_name );
 		if ( ! $config ) {
 			return null;
 		}
@@ -86,11 +84,11 @@ class ConfigStore {
 
 		$data_source_type = null;
 		foreach ( $queries as $query ) {
-			if ( ! $query instanceof HttpQueryContext ) {
+			if ( ! $query instanceof QueryInterface ) {
 				continue;
 			}
 
-			$data_source_type = $query->get_data_source()->get_service();
+			$data_source_type = $query->get_data_source()->get_service_name();
 			if ( $data_source_type ) {
 				break;
 			}
@@ -103,28 +101,18 @@ class ConfigStore {
 	 * Return an unprivileged representation of the data sources that can be
 	 * displayed in settings screens.
 	 *
-	 * @return UiDisplayableInterface[]
+	 * @return DataSourceInterface[]
 	 */
-	public static function get_data_sources_displayable(): array {
+	public static function get_data_sources_as_array(): array {
 		$data_sources = [];
 
-		foreach ( self::$configurations as $config ) {
+		foreach ( self::$blocks as $config ) {
 			foreach ( $config['queries'] as $query ) {
-				if ( ! $query instanceof HttpQueryContext ) {
-					continue;
-				}
-
 				$data_source = $query->get_data_source();
-
-				if ( $data_source instanceof UiDisplayableInterface ) {
-					$data_source_array = $data_source->to_array();
-					if ( isset( $data_source_array['uuid'] ) ) {
-						$data_sources[ $data_source_array['uuid'] ] = $data_source->to_ui_display();
-					}
-				}
+				$data_sources[] = $data_source->to_ui_display();
 			}
 		}
 
-		return array_values( $data_sources );
+		return $data_sources;
 	}
 }
