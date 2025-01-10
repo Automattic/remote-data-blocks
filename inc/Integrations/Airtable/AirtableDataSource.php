5e12bb697ff2c9b843ad55eb971c8de6302d7e91
Shekhar Wagh <shekhar.wagh@automattic.com>
2025-01-10 23:56:31 +0530

    auto-register blocks for each sheet/table
    - Moves the query generation to [Airtable/GoogleSheets]Integration from
      the data source files
    - Auto-register blocks for each sheet/table instead of just the first
      sheet/table in the Airtable or Google Sheets data source
    - Auto-register loop blocks for Airtable in addition to single entity
      blocks

--- inc/Integrations/Airtable/AirtableDataSource.php
+++ inc/Integrations/Airtable/AirtableDataSource.php
@@ -3,9 +3,7 @@
 namespace RemoteDataBlocks\Integrations\Airtable;
 
 use RemoteDataBlocks\Config\DataSource\HttpDataSource;
-use RemoteDataBlocks\Config\Query\HttpQuery;
 use RemoteDataBlocks\Validation\Types;
-use WP_Error;
 
 class AirtableDataSource extends HttpDataSource {
 	protected const SERVICE_NAME = REMOTE_DATA_BLOCKS_AIRTABLE_SERVICE;
@@ -46,72 +44,5 @@ class AirtableDataSource extends HttpDataSource {
 				'Content-Type' => 'application/json',
 			],
 		];
-	}
-
-	public function ___temp_get_query(): HttpQuery|WP_Error {
-		$input_schema = [
-			'record_id' => [
-				'name' => 'Record ID',
-				'type' => 'id',
-			],
-		];
-
-		$output_schema = [
-			'is_collection' => false,
-			'type' => [
-				'id' => [
-					'name' => 'Record ID',
-					'path' => '$.id',
-					'type' => 'id',
-				],
-			],
-		];
-
-		foreach ( $this->config['service_config']['tables'][0]['output_query_mappings'] as $mapping ) {
-			$mapping_key = $mapping['key'];
-			$output_schema['type'][ $mapping_key ] = [
-				'name' => $mapping['name'] ?? $mapping_key,
-				'path' => $mapping['path'] ?? '$.fields["' . $mapping_key . '"]',
-				'type' => $mapping['type'] ?? 'string',
-			];
-		}
-
-		return HttpQuery::from_array( [
-			'data_source' => $this,
-			'endpoint' => function ( array $input_variables ): string {
-				return $this->get_endpoint() . '/' . $this->config['service_config']['tables'][0]['id'] . '/' . $input_variables['record_id'];
-			},
-			'input_schema' => $input_schema,
-			'output_schema' => $output_schema,
-		] );
-	}
-
-	public function ___temp_get_list_query(): HttpQuery|WP_Error {
-		$output_schema = [
-			'is_collection' => true,
-			'path' => '$.records[*]',
-			'type' => [
-				'record_id' => [
-					'name' => 'Record ID',
-					'path' => '$.id',
-					'type' => 'id',
-				],
-			],
-		];
-
-		foreach ( $this->config['service_config']['tables'][0]['output_query_mappings'] as $mapping ) {
-			$output_schema['type'][ $mapping['name'] ] = [
-				'name' => $mapping['name'],
-				'path' => '$.fields.' . $mapping['name'],
-				'type' => $mapping['type'] ?? 'string',
-			];
-		}
-
-		return HttpQuery::from_array( [
-			'data_source' => $this,
-			'endpoint' => $this->get_endpoint() . '/' . $this->config['service_config']['tables'][0]['id'],
-			'input_schema' => [],
-			'output_schema' => $output_schema,
-		] );
 	}
 }
