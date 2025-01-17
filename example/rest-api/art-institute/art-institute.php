38a003ced2a96e64c23968c77aa9bf5a28865f94
Max Schmeling <max.schmeling@automattic.com>
2025-01-17 14:44:54 -0600

    Fix formatting

--- example/rest-api/art-institute/art-institute.php
+++ example/rest-api/art-institute/art-institute.php
@@ -1,14 +1,14 @@
 <?php declare(strict_types = 1);
 
 /**
-* Plugin Name: Art Institute RDB Example
-* Description: Creates a custom block to be used with Remote Data Blocks in order to retrieve artwork from the Art Institute of Chicago.
+ * Plugin Name: Art Institute RDB Example
+ * Description: Creates a custom block to be used with Remote Data Blocks in order to retrieve artwork from the Art Institute of Chicago.
  * Author: WPVIP
  * Author URI: https://remotedatablocks.com/
  * Text Domain: remote-data-blocks
  * Version: 1.0.0
  * Requires Plugins: remote-data-blocks
-*/
+ */
 
 namespace RemoteDataBlocks\Example\ArtInstituteOfChicago;
 
@@ -17,7 +17,7 @@ use RemoteDataBlocks\Config\Query\HttpQuery;
 use function add_query_arg;
 
 function register_aic_block(): void {
-	$aic_data_source = HttpDataSource::from_array( [
+	$aic_data_source = HttpDataSource::from_array([
 		'service_config' => [
 			'__version' => 1,
 			'display_name' => 'Art Institute of Chicago',
@@ -26,9 +26,9 @@ function register_aic_block(): void {
 				'Content-Type' => 'application/json',
 			],
 		],
-	] );
+	]);
 
-	$get_art_query = HttpQuery::from_array( [
+	$get_art_query = HttpQuery::from_array([
 		'data_source' => $aic_data_source,
 		'endpoint' => function ( array $input_variables ) use ( $aic_data_source ): string {
 			return sprintf( '%s/%s', $aic_data_source->get_endpoint(), $input_variables['id'] ?? '' );
@@ -64,9 +64,9 @@ function register_aic_block(): void {
 				],
 			],
 		],
-	] );
+	]);
 
-	$search_art_query = HttpQuery::from_array( [
+	$search_art_query = HttpQuery::from_array([
 		'data_source' => $aic_data_source,
 		'endpoint' => function ( array $input_variables ) use ( $aic_data_source ): string {
 			$query = $input_variables['search_terms'];
@@ -94,9 +94,9 @@ function register_aic_block(): void {
 				],
 			],
 		],
-	] );
+	]);
 
-	register_remote_data_block( [
+	register_remote_data_block([
 		'title' => 'Art Institute of Chicago',
 		'render_query' => [
 			'query' => $get_art_query,
@@ -107,6 +107,6 @@ function register_aic_block(): void {
 				'type' => 'search',
 			],
 		],
-	] );
+	]);
 }
 add_action( 'init', __NAMESPACE__ . '\\register_aic_block' );
--- example/rest-api/zip-code/zip-code.php
+++ example/rest-api/zip-code/zip-code.php
@@ -1,14 +1,14 @@
 <?php declare(strict_types = 1);
 
 /**
-* Plugin Name: Zip Code RDB Example
-* Description: Creates a custom block to be used with Remote Data Blocks in order to retrieve the city and state from a US zip code.
+ * Plugin Name: Zip Code RDB Example
+ * Description: Creates a custom block to be used with Remote Data Blocks in order to retrieve the city and state from a US zip code.
  * Author: WPVIP
  * Author URI: https://remotedatablocks.com/
  * Text Domain: remote-data-blocks
  * Version: 1.0.0
  * Requires Plugins: remote-data-blocks
-*/
+ */
 
 namespace RemoteDataBlocks\Example\ZipCode;
 
@@ -18,17 +18,17 @@ use RemoteDataBlocks\Config\Query\HttpQuery;
 define( 'EXAMPLE_ZIP_CODE_DATA_SOURCE_UUID', 'xxxxxxxx-xxxx-xxxx-xxxxxxxxxxxxxxxxx' );
 
 function register_zipcode_block(): void {
-	if ( ! defined( 'EXAMPLE_ZIP_CODE_DATA_SOURCE_UUID' ) ) {
+	if ( !defined( 'EXAMPLE_ZIP_CODE_DATA_SOURCE_UUID' ) ) {
 		return;
 	}
 
 	$zipcode_data_source = HttpDataSource::from_uuid( EXAMPLE_ZIP_CODE_DATA_SOURCE_UUID );
 
-	if ( ! $zipcode_data_source instanceof HttpDataSource ) {
+	if ( !$zipcode_data_source instanceof HttpDataSource ) {
 		return;
 	}
 
-	$zipcode_query = HttpQuery::from_array( [
+	$zipcode_query = HttpQuery::from_array([
 		'data_source' => $zipcode_data_source,
 		'endpoint' => function ( array $input_variables ) use ( $zipcode_data_source ): string {
 			return $zipcode_data_source->get_endpoint() . $input_variables['zip_code'];
@@ -59,14 +59,14 @@ function register_zipcode_block(): void {
 				],
 			],
 		],
-	] );
+	]);
 
-	register_remote_data_block( [
+	register_remote_data_block([
 		'title' => 'Zip Code',
 		'render_query' => [
 			'query' => $zipcode_query,
 		],
-    'icon' => 'smiley',
-	] );
+		'icon' => 'smiley',
+	]);
 }
 add_action( 'init', __NAMESPACE__ . '\\register_zipcode_block' );
