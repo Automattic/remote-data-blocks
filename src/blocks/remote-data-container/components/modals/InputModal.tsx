b980c4993ba94f8ca3759ef07ea6be2d3ff1fa34
brookewp <brooke.kaminski@automattic.com>
2025-01-17 10:36:06 -0800

    Change button type to accept enter as submit
    Signed-off-by: brookewp <brooke.kaminski@automattic.com>

--- src/blocks/remote-data-container/components/modals/InputModal.tsx
+++ src/blocks/remote-data-container/components/modals/InputModal.tsx
@@ -60,7 +60,7 @@ export function InputModal( props: InputModalProps ) {
 						style={ { marginBottom: '8px' } }
 					/>
 				) ) }
-				<Button variant="primary" onClick={ onSelectItem }>
+				<Button variant="primary" onClick={ onSelectItem } type="submit">
 					{ __( 'Save' ) }
 				</Button>
 			</form>
