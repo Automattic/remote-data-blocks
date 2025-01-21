aab720ac0ca8177a46b6672f8994f7775d0fc563
Alec Geatches <alec.geatches@automattic.com>
2025-01-21 11:23:25 -0700

    Fix type issues related to the edit-post data store

--- types/wordpress__edit-post/index.d.ts
+++ types/wordpress__edit-post/index.d.ts
@@ -1,6 +1,14 @@
+import { StoreDescriptor } from "@wordpress/data";
+
 /**
  * The types provided by @types/wordpress__edit-post are out of date.
  */
+
+interface EditPostStoreDescriptor extends StoreDescriptor {
+	name: 'core/edit-post';
+}
+
+const store: EditPostStoreDescriptor;
 
 declare module '@wordpress/edit-post' {
 	interface EditPostStoreActions {
