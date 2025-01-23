0518dcff758d1cdb62d635f80070bbf5bf501798
Shekhar Wagh <shekhar.wagh@automattic.com>
2025-01-23 15:51:16 +0530

    offload snippet generation to integrations classes

--- inc/Integrations/Shopify/ShopifyIntegration.php
+++ inc/Integrations/Shopify/ShopifyIntegration.php
@@ -136,4 +136,18 @@ class ShopifyIntegration {
 			],
 		] );
 	}
+
+	/**
+	 * Get the block registration snippets for the Shopify integration.
+	 *
+	 * @param array $data_source_config The data source configuration.
+	 * @return array The block registration snippets.
+	 */
+	public static function get_block_registration_snippets( array $data_source_config ): array {
+		$raw_snippet = file_get_contents( __DIR__ . '/templates/block_registration.template' );
+		$snippet = strtr( $raw_snippet, [
+			'{{DATA_SOURCE_UUID}}' => $data_source_config['uuid'],
+		] );
+		return [ $snippet ];
+	}
 }
--- inc/Snippets/Snippets.php
+++ inc/Snippets/Snippets.php
@@ -2,42 +2,38 @@
 
 namespace RemoteDataBlocks\Snippets;
 
-use WP_Error;
 use RemoteDataBlocks\WpdbStorage\DataSourceCrud;
+use RemoteDataBlocks\Integrations\Shopify\ShopifyIntegration;
+use WP_Error;
 
 class Snippets {
 	public static function get_snippets( string $uuid ): array|WP_Error {
-		$data_source_config = DataSourceCrud::get_config_by_uuid( $uuid );
+		$data_source = DataSourceCrud::get_inflated_config_by_uuid( $uuid );
 
-		if ( is_wp_error( $data_source_config ) ) {
-			return $data_source_config;
+		if ( is_wp_error( $data_source ) ) {
+			return $data_source;
 		}
 
+		$data_source_config = $data_source->to_array();
 		$service = $data_source_config['service'];
-
 
 		switch ( $service ) {
 			case 'shopify':
-				return self::get_shopify_snippets( $data_source_config );
+				$snippets = ShopifyIntegration::get_block_registration_snippets( $data_source_config );
+				break;
 			default:
 				return new WP_Error( 'invalid_service', __( 'Invalid service', 'remote-data-blocks' ) );
 		}
+
+		return array_map( [ __CLASS__, 'strip_template_comments' ], $snippets );
 	}
 
-	private static function strip_template_comments( string $content ): string {
+	protected static function strip_template_comments( string $content ): string {
 		// Match PHPDoc blocks that contain @template tags and any preceding blank lines
 		return preg_replace(
 			'/\n*\/\*\*\s*\n\s*\*\s*@template-.*?\*\/\n*/s',
 			"\n\n",
 			$content
 		);
-	}
-
-	private static function get_shopify_snippets( array $data_source_config ): array {
-		$raw_snippet = file_get_contents( __DIR__ . '/Templates/Shopify.template' );
-		$snippet = strtr( $raw_snippet, [
-			'{{DATA_SOURCE_UUID}}' => $data_source_config['uuid'],
-		] );
-		return [ self::strip_template_comments( $snippet ) ];
 	}
 }
