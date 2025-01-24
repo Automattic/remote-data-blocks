ebd477df30f927856f311605afd3effcbe2a4894
chriszarate <chris.zarate@automattic.com>
2025-01-24 11:45:54 -0500

    ui:search schema fixup

--- inc/Validation/ConfigSchemas.php
+++ inc/Validation/ConfigSchemas.php
@@ -170,8 +170,8 @@ final class ConfigSchemas {
 						// types" from our formal schema. Referencing these types allows us
 						// to use the same validation and sanitization logic.
 						//
-						// There are also special types that are not core primitives:
-						// - `ui:search_input` is a string that represents search query input.
+						// There are also special types that are not core primitives, with
+						// accompanying notes.
 						'type' => Types::enum(
 							'boolean',
 							'id',
@@ -179,6 +179,12 @@ final class ConfigSchemas {
 							'null',
 							'number',
 							'string',
+
+							// Special non-primitive types
+
+							// A string that represents search query input. An input variable
+							// with this type must be present for the query to be considered a
+							// search query.
 							'ui:search_input',
 						),
 					] ),
