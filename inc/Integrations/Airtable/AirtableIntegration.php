809f262582830b6e8ac44c4c4eed2584788fea01
brookewp <brooke.kaminski@automattic.com>
2025-01-24 15:30:17 -0800

    Linting
    Signed-off-by: brookewp <brooke.kaminski@automattic.com>

--- inc/Integrations/Airtable/AirtableIntegration.php
+++ inc/Integrations/Airtable/AirtableIntegration.php
@@ -16,7 +16,7 @@ class AirtableIntegration {
 			REMOTE_DATA_BLOCKS_AIRTABLE_SERVICE
 		);
 
-		foreach ( $data_source_configs as $config ) {		
+		foreach ( $data_source_configs as $config ) {       
 			$data_source = AirtableDataSource::from_array( $config );
 
 			if ( $config['service_config']['enable_blocks'] === false ) {
