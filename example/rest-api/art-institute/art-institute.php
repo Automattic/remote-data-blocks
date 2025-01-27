fa44368760603f8edd22cc26e659c8362bc91608
chriszarate <chris.zarate@automattic.com>
2025-01-27 14:06:25 -0500

    Update Art Institute example with ui:search variable

--- example/rest-api/art-institute/art-institute.php
+++ example/rest-api/art-institute/art-institute.php
@@ -69,15 +69,22 @@ function register_aic_block(): void {
 	$search_art_query = HttpQuery::from_array([
 		'data_source' => $aic_data_source,
 		'endpoint' => function ( array $input_variables ) use ( $aic_data_source ): string {
-			$query = $input_variables['search_terms'];
-			$endpoint = $aic_data_source->get_endpoint() . '/search';
+			$endpoint = $aic_data_source->get_endpoint();
+			$search_terms = $input_variables['search'] ?? '';
 
-			return add_query_arg( [ 'q' => $query ], $endpoint );
+			if ( ! empty( $search_terms ) ) {
+				$endpoint = add_query_arg( [ 'q' => $search_terms ], $endpoint . '/search' );
+			}
+
+			return add_query_arg( [
+				'limit' => $input_variables['limit'],
+				'page' => $input_variables['page'],
+			], $endpoint );
 		},
 		'input_schema' => [
-			'search_terms' => [
-				'name' => 'Search Terms',
-				'type' => 'string',
+			'search' => [
+				'name' => 'Search terms',
+				'type' => 'ui:search_input',
 			],
 		],
 		'output_schema' => [
