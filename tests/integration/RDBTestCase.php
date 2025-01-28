eb92122f5a76f3105ee04a68b12c8d5a4c46c87a
Alec Geatches <alec.geatches@automattic.com>
2025-01-28 10:20:11 -0700

    @psalm-suppress everything in RDBTestCase

--- tests/integration/RDBTestCase.php
+++ tests/integration/RDBTestCase.php
@@ -10,6 +10,9 @@ use RemoteDataBlocks\Editor\BlockManagement\ConfigStore;
 class RDBTestCase extends WP_UnitTestCase {
 	// Query mocking
 
+	/**
+	 * @psalm-suppress PossiblyUnusedParam
+	 */
 	protected function register_mocked_data_block( string $block_title, array $api_response, array $output_schema ): void {
 		$test_query_runner = $this->get_query_runner_with_response( $api_response );
 
@@ -44,6 +47,9 @@ class RDBTestCase extends WP_UnitTestCase {
 		BlockRegistration::register_block_configuration( $block_config );
 	}
 
+	/**
+	 * @psalm-suppress PossiblyUnusedParam
+	 */
 	protected function register_failed_query_data_block( string $block_title ): void {
 		$test_query_runner = $this->get_query_runner_with_response( [], 500 );
 
@@ -92,6 +98,9 @@ class RDBTestCase extends WP_UnitTestCase {
 		BlockRegistration::register_block_configuration( $block_config );
 	}
 
+	/**
+	 * @psalm-suppress PossiblyUnusedParam
+	 */
 	protected function get_query_runner_with_response( array $response_data, int $status_code = 200 ): QueryRunner {
 		return new class($response_data, $status_code) extends QueryRunner {
 			private $response_data;
@@ -116,6 +125,9 @@ class RDBTestCase extends WP_UnitTestCase {
 
 	// DOM testing
 
+	/**
+	 * @psalm-suppress PossiblyUnusedParam
+	 */
 	protected function load_html( string $html ): DOMDocument {
 		$dom = new DOMDocument();
 
@@ -128,6 +140,9 @@ class RDBTestCase extends WP_UnitTestCase {
 		return $dom;
 	}
 
+	/**
+	 * @psalm-suppress PossiblyUnusedParam
+	 */
 	public function assertDomIdHasTextContent( DOMDocument $dom, string $html_id, string $expected_content ): void {
 		$xpath = new DOMXPath( $dom );
 		$id_nodes = $xpath->query( sprintf( "//*[@id='%s']", $html_id ) );
@@ -136,6 +151,9 @@ class RDBTestCase extends WP_UnitTestCase {
 		$this->assertEquals( $expected_content, $id_nodes[0]->textContent, sprintf( "Expected '%s' in node with HTML ID '%s', but found '%s' instead.", $expected_content, $html_id, $id_nodes[0]->textContent ) );
 	}
 
+	/**
+	 * @psalm-suppress PossiblyUnusedParam
+	 */
 	protected function assertDomIdHasHtmlContent( DOMDocument $dom, string $html_id, string $expected_content ): void {
 		$xpath = new DOMXPath( $dom );
 		$id_nodes = $xpath->query( sprintf( "//*[@id='%s']", $html_id ) );
