833f49a657f9fc310058cfe10377b23b7390435b
Shekhar Wagh <shekhar.wagh@automattic.com>
2025-01-29 18:25:12 +0530

    add snippet generation for salesforce b2c

--- inc/Integrations/SalesforceB2C/SalesforceB2CIntegration.php
+++ inc/Integrations/SalesforceB2C/SalesforceB2CIntegration.php
@@ -182,4 +182,18 @@ class SalesforceB2CIntegration {
 			return $input_variables;
 		}, 10, 2 );
 	}
+
+	/**
+	 * Get the block registration snippets for the Salesforce B2C integration.
+	 *
+	 * @param array $data_source_config The data source configuration.
+	 * @return array The block registration snippets.
+	 */
+	public static function get_block_registration_snippets( array $data_source_config ): array {
+		$raw_snippet = file_get_contents( __DIR__ . '/templates/block_registration.template' );
+		$snippet = strtr( $raw_snippet, [
+			'{{DATA_SOURCE_UUID}}' => $data_source_config['uuid'],
+		] );
+		return [ $snippet ];
+	}
 }
--- inc/Snippets/Snippets.php
+++ inc/Snippets/Snippets.php
@@ -6,6 +6,7 @@ use RemoteDataBlocks\WpdbStorage\DataSourceCrud;
 use RemoteDataBlocks\Integrations\Airtable\AirtableIntegration;
 use RemoteDataBlocks\Integrations\Google\Sheets\GoogleSheetsIntegration;
 use RemoteDataBlocks\Integrations\Shopify\ShopifyIntegration;
+use RemoteDataBlocks\Integrations\SalesforceB2C\SalesforceB2CIntegration;
 use WP_Error;
 
 class Snippets {
@@ -28,6 +29,9 @@ class Snippets {
 				break;
 			case 'google-sheets':
 				$snippets = GoogleSheetsIntegration::get_block_registration_snippets( $data_source_config );
+				break;
+			case 'salesforce-b2c':
+				$snippets = SalesforceB2CIntegration::get_block_registration_snippets( $data_source_config );
 				break;
 			default:
 				return new WP_Error( 'invalid_service', __( 'Invalid service', 'remote-data-blocks' ) );
