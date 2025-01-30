3e7564af4770233eb19891e96cb152bb006de0c1
brookewp <brooke.kaminski@automattic.com>
2025-01-30 12:14:50 -0800

    Remove unused arg
    Signed-off-by: brookewp <brooke.kaminski@automattic.com>

--- inc/Integrations/Airtable/AirtableDataSource.php
+++ inc/Integrations/Airtable/AirtableDataSource.php
@@ -17,7 +17,7 @@ class AirtableDataSource extends HttpDataSource {
 				'id' => Types::string(),
 				'name' => Types::nullable( Types::string() ),
 			] ),
-			'enable_blocks' => Types::nullable( Types::boolean(), true ),
+			'enable_blocks' => Types::nullable( Types::boolean() ),
 			'display_name' => Types::string(),
 			'tables' => Types::list_of(
 				Types::object( [
