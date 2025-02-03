68bf20c59e519249034e15ddcac90b0a574c2999
ingeniumed <ingeniumed@users.noreply.github.com>
2025-02-03 14:08:43 +1100

    Fix incorrect paths

--- .github/workflows/scripts/get-plugin-version.js
+++ .github/workflows/scripts/get-plugin-version.js
@@ -4,7 +4,7 @@ const { join } = require( 'path' );
 exports.getVersion = async plugin => {
 	const filePath = join(
 		process.env.GITHUB_WORKSPACE,
-		`plugins/${ plugin }/${ plugin }.php`
+		`${ plugin }.php`
 	);
 	const pluginFileContents = await readFile( filePath, 'utf8' );
 	const versionMatch = pluginFileContents.match( /Version: (\d+\.\d+\.\d+.*)\n/m );
