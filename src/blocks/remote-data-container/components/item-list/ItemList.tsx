eed54e2dedff4db8b5610738dbfec724f7605f56
chriszarate <chris.zarate@automattic.com>
2025-02-05 18:12:25 -0500

    Refactor ItemList to remove memos

--- src/blocks/remote-data-container/components/item-list/ItemList.tsx
+++ src/blocks/remote-data-container/components/item-list/ItemList.tsx
@@ -1,9 +1,10 @@
 import { useInstanceId } from '@wordpress/compose';
 import { DataViews, View } from '@wordpress/dataviews/wp';
-import { useMemo, useState } from '@wordpress/element';
+import { useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 import { usePatterns } from '@/blocks/remote-data-container/hooks/usePatterns';
+import { removeNullValuesFromObject } from '@/utils/type-narrowing';
 
 interface ItemListProps {
 	availableBindings: Record< string, RemoteDataBinding >;
@@ -19,6 +20,23 @@ interface ItemListProps {
 	supportsSearch: boolean;
 	totalItems?: number;
 	totalPages?: number;
+}
+
+function getResultsWithId( results: RemoteDataResult[], instanceId: string ): RemoteDataResult[] {
+	return ( results ?? [] ).map( ( result: RemoteDataResult ) => {
+		const parsedItem = removeNullValuesFromObject( result );
+
+		if ( parsedItem.id ) {
+			return parsedItem;
+		}
+
+		// ensure each result has an 'id' key
+		const idKey = Object.keys( parsedItem ).find( key => /(^|_)(id)$/i.test( key ) );
+		return {
+			...parsedItem,
+			id: idKey ? parsedItem[ idKey ] : instanceId,
+		};
+	} );
 }
 
 export function ItemList( props: ItemListProps ) {
@@ -40,80 +58,49 @@ export function ItemList( props: ItemListProps ) {
 	const { defaultPattern: pattern } = usePatterns( blockName );
 
 	const instanceId = useInstanceId( ItemList, blockName );
-
-	const data = useMemo( () => {
-		if ( loading ) {
-			return [];
-		}
-
-		// remove null values from the data to prevent errors in filterSortAndPaginate
-		const removeNullValues = ( obj: Record< string, unknown > ): Record< string, unknown > => {
-			return Object.fromEntries(
-				Object.entries( obj ).filter( ( [ _, value ] ) => value !== null )
-			);
-		};
-
-		return ( results ?? [] ).map( ( item: Record< string, unknown > ) => {
-			const parsedItem = removeNullValues( item );
-
-			if ( parsedItem.id ) {
-				return parsedItem;
-			}
-
-			// ensure each result has an 'id' key
-			const idKey = Object.keys( parsedItem ).find( key => /(^|_)(id)$/i.test( key ) );
-			return {
-				...parsedItem,
-				id: idKey ? parsedItem[ idKey ] : instanceId,
-			};
-		} ) as RemoteDataResult[];
-	}, [ results, loading ] );
+	const data = loading ? [] : getResultsWithId( results ?? [], instanceId );
 
 	// get fields from results data to use as columns
-	const { fields, mediaField, tableFields, titleField } = useMemo( () => {
-		const getFields: string[] = Array.from(
-			new Set(
-				data
-					?.flatMap( item => Object.keys( item ) )
-					.filter(
-						key => key in availableBindings && availableBindings[ key ]?.type !== 'id' // filter out ID fields to hide from table
-					)
-			)
-		);
+	const fieldNames: string[] = Array.from(
+		new Set(
+			data
+				?.flatMap( item => Object.keys( item ) )
+				.filter(
+					key => key in availableBindings && availableBindings[ key ]?.type !== 'id' // filter out ID fields to hide from table
+				)
+		)
+	);
 
-		// Find title field from availableBindings by checking type
-		const title = Object.entries( availableBindings ).find(
-			( [ _, binding ] ) => binding.type === 'string' && binding.name.toLowerCase() === 'title'
-		)?.[ 0 ];
+	// Find title field from availableBindings by checking type
+	const titleField = Object.entries( availableBindings ).find(
+		( [ _, binding ] ) => binding.type === 'string' && binding.name.toLowerCase() === 'title'
+	)?.[ 0 ];
 
-		// Find media field from availableBindings by checking type
-		const media = Object.entries( availableBindings ).find(
-			( [ _, binding ] ) => binding.type === 'image_url'
-		)?.[ 0 ];
+	// Find media field from availableBindings by checking type
+	const mediaField = Object.entries( availableBindings ).find(
+		( [ _, binding ] ) => binding.type === 'image_url'
+	)?.[ 0 ];
 
-		const fieldObject = getFields.map( field => {
-			return {
-				id: field,
-				label: availableBindings[ field ]?.name ?? field,
-				enableGlobalSearch: true,
-				getValue: ( { item }: { item: RemoteDataResult } ) => item[ field ] as string,
-				render:
-					field === media
-						? ( { item }: { item: RemoteDataResult } ) => {
-								return (
-									<img alt={ ( item.image_alt as string ) ?? '' } src={ item[ field ] as string } />
-								);
-						  }
-						: undefined,
-				enableSorting: field !== media,
-			};
-		} );
+	const fields = fieldNames.map( field => {
+		return {
+			id: field,
+			label: availableBindings[ field ]?.name ?? field,
+			enableGlobalSearch: true,
+			getValue: ( { item }: { item: RemoteDataResult } ) => item[ field ] as string,
+			render:
+				field === mediaField
+					? ( { item }: { item: RemoteDataResult } ) => {
+							return (
+								<img alt={ ( item.image_alt as string ) ?? '' } src={ item[ field ] as string } />
+							);
+					  }
+					: undefined,
+			enableSorting: field !== mediaField,
+		};
+	} );
 
-		// hide media and title fields from table view if defined to avoid duplication
-		const filtered = getFields.filter( field => field !== media && field !== title );
-
-		return { fields: fieldObject, tableFields: filtered, titleField: title, mediaField: media };
-	}, [ availableBindings, data ] );
+	// hide media and title fields from table view if defined to avoid duplication
+	const tableFields = fieldNames.filter( field => field !== mediaField && field !== titleField );
 
 	const [ view, setView ] = useState< View >( {
 		type: 'table' as const,
