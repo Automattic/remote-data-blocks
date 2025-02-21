94ce3a15923f467e0ceb8218b8058aeff2ed4da3
Alec Geatches <alec.geatches@automattic.com>
2025-02-21 13:27:14 -0700

    Expand error for missing output key in query that's required for another query's input

--- inc/Editor/BlockManagement/ConfigRegistry.php
+++ inc/Editor/BlockManagement/ConfigRegistry.php
@@ -96,7 +96,7 @@ class ConfigRegistry {
 
 			foreach ( array_keys( $to_query->get_input_schema() ) as $to ) {
 				if ( ! isset( $from_output_schema['type'][ $to ] ) ) {
-					return self::create_error( $block_title, sprintf( 'Cannot map key "%s" from %s query', esc_html( $to ), $from_query_type ) );
+					return self::create_error( $block_title, sprintf( 'Cannot map key "%1$s" from %2$s query. The display query requires a "%1$s" key as an input, but it is not present in the output schema for the %2$s query. Try adding "%1$s" to the output schema for the %2$s query.', esc_html( $to ), $from_query_type, $from_query_type, esc_html( $to ) ) );
 				}
 			}
 
