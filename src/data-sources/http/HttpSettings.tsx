37979fcfcc119e0f27213b8c1248e1374ee6d8eb
ingeniumed <ingeniumed@users.noreply.github.com>
2025-03-04 14:42:50 +1100

    Fix the linting issues

--- src/data-sources/http/HttpSettings.tsx
+++ src/data-sources/http/HttpSettings.tsx
@@ -1,6 +1,7 @@
 import { TextControl } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
+import { DataSourceForm } from '../components/DataSourceForm';
 import { HttpAuthSettingsInput } from '@/data-sources/components/HttpAuthSettingsInput';
 import { ConfigSource } from '@/data-sources/constants';
 import { useDataSources } from '@/data-sources/hooks/useDataSources';
@@ -8,7 +9,6 @@ import { HttpAuth } from '@/data-sources/http/types';
 import { HttpConfig, HttpServiceConfig, SettingsComponentProps } from '@/data-sources/types';
 import { useForm } from '@/hooks/useForm';
 import HttpIcon from '@/settings/icons/HttpIcon';
-import { DataSourceForm } from '../components/DataSourceForm';
 
 const SERVICE_CONFIG_VERSION = 1;
 
