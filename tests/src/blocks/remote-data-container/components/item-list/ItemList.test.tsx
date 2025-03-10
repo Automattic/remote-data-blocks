966819637852547291d4b45366be3463a63166a4
brookewp <brooke.kaminski@automattic.com>
2025-03-10 15:25:25 -0700

    Update saved values to align with merged changes
    Signed-off-by: brookewp <brooke.kaminski@automattic.com>

--- tests/src/blocks/remote-data-container/components/item-list/ItemList.test.tsx
+++ tests/src/blocks/remote-data-container/components/item-list/ItemList.test.tsx
@@ -55,7 +55,14 @@ const defaultProps = {
 };
 
 const ItemListComponent = ( props: ItemListProps ) => {
-	const [ selectionIds, setSelectionIds ] = useState< string[] >( [] );
+	const [ selection, setSelection ] = useState< RemoteDataApiResult[] >( [] );
+	const selectionIds = selection.map( item => item.uuid );
+	function setSelectionIds( uuids: string[] ): void {
+		const newSelection = uuids
+			.map( uuid => props.results?.find( result => result.uuid === uuid ) )
+			.filter( ( item ): item is RemoteDataApiResult => item !== undefined );
+		setSelection( newSelection );
+	}
 	return (
 		<>
 			<ItemList
@@ -65,7 +72,7 @@ const ItemListComponent = ( props: ItemListProps ) => {
 				onSelect={ props.onSelect }
 			/>
 			<Button onClick={ () => setSelectionIds( [] ) }>Cancel</Button>
-			<Button onClick={ () => props.onSelect( selectionIds ) }>Save</Button>
+			<Button onClick={ () => props.onSelect?.( selection ) }>Save</Button>
 		</>
 	);
 };
@@ -136,7 +143,30 @@ describe( 'ItemList', () => {
 		await user.click( saveButton );
 
 		// Verify onSelect was called with the correct items
-		expect( onSelect ).toHaveBeenCalledWith( expect.arrayContaining( [ 'violets', 'poppy' ] ) );
+		expect( onSelect ).toHaveBeenCalledWith(
+			expect.arrayContaining( [
+				{
+					result: {
+						title: {
+							name: 'Title',
+							type: 'title',
+							value: 'Violets',
+						},
+					},
+					uuid: 'violets',
+				},
+				{
+					result: {
+						title: {
+							name: 'Title',
+							type: 'title',
+							value: 'Poppy',
+						},
+					},
+					uuid: 'poppy',
+				},
+			] )
+		);
 	} );
 
 	it( 'should allow deselection of items', async () => {
@@ -169,7 +199,20 @@ describe( 'ItemList', () => {
 		await user.click( saveButton );
 
 		// Verify onSelect was called with the correct item
-		expect( onSelect ).toHaveBeenCalledWith( expect.arrayContaining( [ 'poppy' ] ) );
+		expect( onSelect ).toHaveBeenCalledWith(
+			expect.arrayContaining( [
+				{
+					result: {
+						title: {
+							name: 'Title',
+							type: 'title',
+							value: 'Poppy',
+						},
+					},
+					uuid: 'poppy',
+				},
+			] )
+		);
 	} );
 
 	it( 'should render pagination buttons when there is more than one page', async () => {
