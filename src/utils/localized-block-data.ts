550d76dc2269606e253cc6f719364a7d0f45c9d4
ingeniumed <ingeniumed@users.noreply.github.com>
2025-04-08 11:07:45 +1000

    Give back the block name when the title cannot be found

--- src/utils/localized-block-data.ts
+++ src/utils/localized-block-data.ts
@@ -14,12 +14,23 @@ export function getBlockDataSourceType( blockName?: string ): string {
 	return getBlockConfig( blockName )?.dataSourceType ?? '';
 }
 
+/**
+ * Get the title of a remote data block.
+ *
+ * The title could be set in the block config or it could be the block name, without the remote-data-blocks/ prefix.
+ * If the title is not found, or the block name is not found, then unknown is returned.
+ *
+ * @param blockName The name of the remote data block.
+ * @returns The title of the remote data block.
+ */
 export function getBlockTitle( blockName?: string ): string {
 	if ( ! blockName ) {
 		return '';
 	}
 
-	return getBlockConfig( blockName )?.settings?.title ?? '';
+	return (
+		getBlockConfig( blockName )?.settings?.title ?? blockName.replace( 'remote-data-blocks/', '' )
+	);
 }
 
 export function getBlocksConfig(): BlocksConfig {
