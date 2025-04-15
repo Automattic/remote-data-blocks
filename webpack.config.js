4f54fca1e2081d120e5135caec5f5395897d61e8
Shekhar Wagh <shekhar.wagh@automattic.com>
2025-04-15 18:38:36 +0530

    ignore node_modules for webpack watch
    The command to start local server was failing with EMFILE errors. Ignoring node_modules and increasing the aggregateTimeout in watchOptions fixed the issue for now. The files in node_modules won't be changed during development without any dependencies changing, so it's safe to ignore them for watching.

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,21 @@ const additionalScripts = {
 
 const { modernize, moduleConfig, scriptConfig } = require( './webpack.utils' );
 
+// Add watchOptions configuration to reduce file watching load
+const watchOptions = {
+	ignored: /node_modules/,
+	aggregateTimeout: 300,
+};
+
 module.exports = [
-	modernize( scriptConfig, additionalScripts, [
-		// we only need to fork one copy of ts-checker off here in these webpack exports
-		new ForkTsCheckerWebpackPlugin(),
-	] ),
-	modernize( moduleConfig ),
+	modernize(
+		scriptConfig,
+		additionalScripts,
+		[
+			// we only need to fork one copy of ts-checker off here in these webpack exports
+			new ForkTsCheckerWebpackPlugin(),
+		],
+		watchOptions
+	),
+	modernize( moduleConfig, {}, [], watchOptions ),
 ];
