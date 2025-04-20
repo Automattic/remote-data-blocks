1fa719bba45ca856c9eba93d9275cfe750d418e5
chriszarate <chris.zarate@automattic.com>
2025-04-20 12:09:05 -0400

    Add class_exists check

--- inc/Logging/QueryMonitor/RdbValidationIssueOutputHtml.php
+++ inc/Logging/QueryMonitor/RdbValidationIssueOutputHtml.php
@@ -8,43 +8,39 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-/**
- * @psalm-suppress UndefinedClass,UnusedClass
- */
-class RdbValidationIssueOutputHtml extends QM_Output_Html_Logger {
-	public static string $collector_id = 'remote-data-blocks-validation';
+if ( class_exists( 'QM_Output_Html_Logger' ) ) {
+	class RdbValidationIssueOutputHtml extends QM_Output_Html_Logger {
+		public static string $collector_id = 'remote-data-blocks-validation';
 
-	/**
-	 * @psalm-suppress PossiblyUnusedParam
-	 */
-	public function admin_menu( array $menu ): array {
-		/** @var QM_Data_Logger $data */
-		$data = $this->collector->get_data();
-		$count = 0;
+		public function admin_menu( array $menu ): array {
+			/** @var QM_Data_Logger $data */
+			$data = $this->collector->get_data();
+			$count = 0;
 
-		if ( ! empty( $data->logs ) ) {
-			$count = count( $data->logs );
+			if ( ! empty( $data->logs ) ) {
+				$count = count( $data->logs );
 
-			/* translators: %s: Number of logs that are available */
-			$label = __( 'Validation issues (%s)', 'query-monitor' );
-		} else {
-			$label = __( 'Validation issues', 'query-monitor' );
+				/* translators: %s: Number of logs that are available */
+				$label = __( 'Validation issues (%s)', 'query-monitor' );
+			} else {
+				$label = __( 'Validation issues', 'query-monitor' );
+			}
+
+			if ( ! isset( $menu['remote-data-blocks'] ) ) {
+					$menu['remote-data-blocks'] = $this->menu( [
+						'title' => __( 'Remote Data Blocks', 'query-monitor' ),
+					] );
+			}
+
+			$menu['remote-data-blocks']['children'][ $this->collector->id() ] = $this->menu( array(
+				'id' => 'remote-data-blocks-validation',
+				'title' => esc_html( sprintf(
+					$label,
+					number_format_i18n( $count )
+				) ),
+			) );
+
+			return $menu;
 		}
-
-		if ( ! isset( $menu['remote-data-blocks'] ) ) {
-				$menu['remote-data-blocks'] = $this->menu( [
-					'title' => __( 'Remote Data Blocks', 'query-monitor' ),
-				] );
-		}
-
-		$menu['remote-data-blocks']['children'][ $this->collector->id() ] = $this->menu( array(
-			'id' => 'remote-data-blocks-validation',
-			'title' => esc_html( sprintf(
-				$label,
-				number_format_i18n( $count )
-			) ),
-		) );
-
-		return $menu;
 	}
 }
