0b1b6a2d0b2b9d216347632182b2aeff94fe7118
chriszarate <chris.zarate@automattic.com>
2025-04-20 11:53:06 -0400

    Remove unused warning detection

--- inc/Logging/QueryMonitor/RdbValidationIssueOutputHtml.php
+++ inc/Logging/QueryMonitor/RdbValidationIssueOutputHtml.php
@@ -23,13 +23,6 @@ class RdbValidationIssueOutputHtml extends QM_Output_Html_Logger {
 		$count = 0;
 
 		if ( ! empty( $data->logs ) ) {
-			foreach ( $data->logs as $log ) {
-				if ( in_array( $log['level'], $this->collector->get_warning_levels(), true ) ) {
-					$key = 'warning';
-					break;
-				}
-			}
-
 			$count = count( $data->logs );
 
 			/* translators: %s: Number of logs that are available */
