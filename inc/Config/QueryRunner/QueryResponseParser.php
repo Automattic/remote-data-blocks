9ee70f304bf568becbe210592ed337da02668e89
chriszarate <chris.zarate@automattic.com>
2025-05-02 18:10:56 -0600

    Do not sanitize null value

--- inc/Config/QueryRunner/QueryResponseParser.php
+++ inc/Config/QueryRunner/QueryResponseParser.php
@@ -13,15 +13,18 @@ defined( 'ABSPATH' ) || exit();
  */
 final class QueryResponseParser {
 	/**
-	 * Get a field value based on its type. This should be a primitive type
+	 * Get a field value based on its type. This should be a primitive type.
 	 *
 	 * @param mixed $field_value The field value.
 	 * @param string $type_name The field type.
-	 * @param mixed  $default_value The default value.
 	 * @return mixed The sanitized field value.
 	 */
-	private function get_field_value( mixed $field_value, string $type_name, mixed $default_value = null ): mixed {
-		$sanitized_value = Sanitizer::sanitize_primitive_type( $type_name, $field_value ?? $default_value );
+	private function get_field_value( mixed $field_value, string $type_name ): mixed {
+		if ( null === $field_value ) {
+			return null;
+		}
+
+		$sanitized_value = Sanitizer::sanitize_primitive_type( $type_name, $field_value );
 
 		// Some fields get formatted based on their type.
 		switch ( $type_name ) {
@@ -64,10 +67,10 @@ final class QueryResponseParser {
 		} elseif ( is_string( $schema['type'] ?? null ) ) {
 			if ( $is_collection ) {
 				$value = array_map( function ( $item ) use ( $schema ) {
-					return $this->get_field_value( $item, $schema['type'], $schema['default_value'] ?? null );
+					return $this->get_field_value( $item ?? $schema['default_value'] ?? null, $schema['type'] );
 				}, $value );
 			} else {
-				$value = [ $this->get_field_value( $value[0] ?? null, $schema['type'], $schema['default_value'] ?? null ) ];
+				$value = [ $this->get_field_value( $value[0] ?? $schema['default_value'] ?? null, $schema['type'] ) ];
 			}
 		} else {
 			$value = [];
