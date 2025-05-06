206b3c44ed1b03f0181b7454c049e35da8a2b0f1
ingeniumed <ingeniumed@users.noreply.github.com>
2025-05-06 11:04:09 +1000

    Add a test

--- tests/inc/Integrations/GenericHttp/GenericHttpDataSourceTest.php
+++ tests/inc/Integrations/GenericHttp/GenericHttpDataSourceTest.php
@@ -6,7 +6,8 @@ use PHPUnit\Framework\TestCase;
 use RemoteDataBlocks\Integrations\GenericHttp\GenericHttpDataSource;
 
 class GenericHttpDataSourceTest extends TestCase {
-	public function testGetServiceMethodReturnsCorrectValue(): void {
+
+	public function test_get_service_method_returns_correct_value(): void {
 		$config = [
 			'service_config' => [
 				'__version' => 1,
@@ -17,5 +18,31 @@ class GenericHttpDataSourceTest extends TestCase {
 		$data_source = GenericHttpDataSource::from_array( $config );
 
 		$this->assertEquals( 'generic-http', $data_source->get_service_name() );
+	}
+
+	public function test_to_array_returns_correctly_mapped_values(): void {
+		$config = [
+			'service_config' => [
+				'__version' => 1,
+				'display_name' => 'Mock Data Source',
+				'endpoint' => 'http://example.com',
+				'auth' => [
+					'type' => 'basic',
+					'key' => 'api_key',
+					'value' => '1234567890',
+				],
+			],
+		];
+
+		// By calling to_array, we can ensure that the config schema is correctly defined,
+		// and that values haven't been sanitized due to them missing from the schema.
+		$data_source = GenericHttpDataSource::from_array( $config );
+		$data_source_array = $data_source->to_array();
+
+		$this->assertEquals( 'generic-http', $data_source_array['service'] );
+		$this->assertEquals( 'http://example.com', $data_source_array['endpoint'] );
+		$this->assertEquals( [
+			'Authorization' => 'Basic MTIzNDU2Nzg5MA==',
+		], $data_source_array['request_headers'] );
 	}
 }
