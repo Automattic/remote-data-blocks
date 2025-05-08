cab9e62fe2d5a5cb71146f7b539c58415caf5e4e
ingeniumed <ingeniumed@users.noreply.github.com>
2025-05-08 11:08:13 +1000

    Fix the icons for the variations

--- src/blocks/remote-data-no-results/index.ts
+++ src/blocks/remote-data-no-results/index.ts
@@ -1,4 +1,4 @@
-import { registerBlockType, registerBlockVariation } from '@wordpress/blocks';
+import { registerBlockType } from '@wordpress/blocks';
 import { border, caution } from '@wordpress/icons';
 
 import metadata from './block.json';
@@ -6,19 +6,18 @@ import { Edit } from './edit';
 import { Save } from './save';
 import './style.scss';
 
+// @ts-expect-error - WordPress registerBlockType type definition does not include variations even though this is the right way to do it.
 registerBlockType< RemoteDataNoResultsBlockAttributes >( metadata.name, {
 	edit: Edit,
 	icon: {
 		src: border,
 	},
 	save: Save,
-} );
-
-registerBlockVariation( metadata.name, {
-	name: 'remote-data-blocks/error',
-	title: 'Error',
-	icon: {
-		src: caution,
-	},
-	attributes: { mode: 'error' },
+	variations: [
+		{
+			name: 'remote-data-blocks/error',
+			title: 'Error',
+			icon: { src: caution },
+		},
+	],
 } );
