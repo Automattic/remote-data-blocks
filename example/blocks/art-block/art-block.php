0d024aa4de841ce3e97845497c508d3390cff481
chriszarate <chris.zarate@automattic.com>
2025-06-06 11:50:18 -0600

    Add comment to art block search term handling

--- example/blocks/art-block/art-block.php
+++ example/blocks/art-block/art-block.php
@@ -87,6 +87,8 @@ function register_art_remote_data_block(): void {
 			$endpoint = $aic_data_source['endpoint'] . '/search';
 			$search_terms = $input_variables['search'] ?? '';
 
+			// Do not include the `q` parameter if the search terms are empty.
+			// Otherwise, this will result in an error from the API.
 			if ( ! empty( $search_terms ) ) {
 				$endpoint = add_query_arg( [ 'q' => $search_terms ], $endpoint );
 			}
