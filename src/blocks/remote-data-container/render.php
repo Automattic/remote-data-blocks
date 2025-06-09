c2306d8b6d42e1d55aac86de774d0c570444ecb6
Paul Kevan <paul.kevan@automattic.com>
2025-06-09 11:37:30 +0100

    Check display_name structure before assigning

--- src/blocks/remote-data-container/render.php
+++ src/blocks/remote-data-container/render.php
@@ -6,5 +6,10 @@ use RemoteDataBlocks\Editor\DataBinding\BlockBindings;
 // $attributes (array): The block attributes.
 // $content (string): The block default content.
 // $block (WP_Block): The block instance.
+global $attributes, $content, $block;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
 
 echo wp_kses_post( BlockBindings::render_remote_data_block( $attributes, $content, $block ) );
