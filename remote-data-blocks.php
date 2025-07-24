8432d61b49650f6e256feb83bd54d7991a649075
Paul Kevan <2290623+pkevan@users.noreply.github.com>
2025-07-24 11:56:31 +0100

    Add license to plugin
    See #616.

--- remote-data-blocks.php
+++ remote-data-blocks.php
@@ -10,6 +10,8 @@
  * Version: 1.0.2
  * Requires at least: 6.7
  * Requires PHP: 8.1
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
 namespace RemoteDataBlocks;
