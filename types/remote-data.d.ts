e36234bc7b2bb7823b97fe2f17b76f0c099e5927
Paul Kevan <paul.kevan@automattic.com>
2025-10-03 15:17:14 +0100

    :shrug:

--- types/remote-data.d.ts
+++ types/remote-data.d.ts
@@ -97,7 +97,7 @@ interface RemoteDataApiRequest {
 	block_name: string;
 	query_inputs: RemoteDataQueryInput[];
 	query_key: string;
-	post_id: ?number | string;
+	post_id: ?( number | string );
 }
 
 interface RemoteDataApiResult {
