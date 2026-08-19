78741db799a94c23a58bab278afe613d3bc0ae2e
Max Schmeling <max.schmeling@automattic.com>
2026-08-19 09:59:13 -0500

    Remove unnecessary __invoke

--- inc/HttpClient/RdbCacheMiddleware.php
+++ inc/HttpClient/RdbCacheMiddleware.php
@@ -5,14 +5,6 @@ namespace RemoteDataBlocks\HttpClient;
 use Psr\Http\Message\RequestInterface;
 
 class RdbCacheMiddleware extends \Kevinrob\GuzzleCache\CacheMiddleware {
-	public function __invoke( callable $handler ): callable {
-		$handler_without_cache_metadata = function ( RequestInterface $request, array $options ) use ( $handler ) {
-			return $handler( $request->withoutHeader( RdbCacheStrategy::CACHE_KEY_REQUEST_HEADERS_REQUEST_HEADER ), $options );
-		};
-
-		return parent::__invoke( $handler_without_cache_metadata );
-	}
-
 	/**
 	 * @var array<string, true>
 	 */
